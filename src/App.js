2d303a374cdb0dba0c8556760ed6e34f27677722
CutyApple <dbtldhs0915@naver.com>
2021-02-02 21:32:59 +0900

    [FEAT] <src/App.js> init file

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStyle } from "./GlobalStyle";
+import Routing from "./Routing";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routing />
+    </BrowserRouter>
+  );
+};
+
+export default App;
