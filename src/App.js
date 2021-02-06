2a195cba67cb58eef8cd00da44bde6e1224e77b9
JaeHyun <soehd0889@naver.com>
2021-02-06 22:05:50 +0900

    .

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./GlobalStyle";
 import Routing from "./Routing";
+import Header from "./component/Header/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routing />
+      <Header />
     </BrowserRouter>
   );
 };
