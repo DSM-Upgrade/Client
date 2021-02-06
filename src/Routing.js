c336d69789d2f386aa8faf3a70099ffe4b975641
CutyApple <dbtldhs0915@naver.com>
2021-02-07 04:27:24 +0900

    [FEAT] <src/Routing.js> apply "/mypage" to MyPageContainer

--- src/Routing.js
+++ src/Routing.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import MyPageContanier from "./container/MyPageContanier/MyPageContanier";
 
 const Routing = () => {
   return (
     <>
-      <Switch></Switch>
+      <Switch>
+        <Route exact path="/mypage" component={MyPageContanier} />
+      </Switch>
     </>
   );
 };
