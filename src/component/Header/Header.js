3a70564fd15bd133e023eba82b1599fcfb5d1b50
JaeHyun <soehd0889@naver.com>
2021-02-07 16:00:54 +0900

    [FEAT] <Header.js , style.js> Header 완성

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -5,26 +5,41 @@ const Header = () => {
   return (
     <S.Header>
       <S.MainBox>
-        <S.LogoTitleBox>
+        <S.LogoBox>
           <S.Text>DMS - </S.Text>
-          <S.Text style={{marginLeft: "6px"}} Blue> U</S.Text>
+          <S.Text style={{ marginLeft: "6px" }} Blue>
+            U
+          </S.Text>
           <S.Text>P</S.Text>
-        </S.LogoTitleBox>
-        <S.LogoTitleBox>
+        </S.LogoBox>
+        <S.LogoBox>
           <S.Text Blue Title>
             U
           </S.Text>
           <S.Text Title>pgrade</S.Text>
-        </S.LogoTitleBox>
-        <S.LogoTitleBox Login>
-          <a style={{margin: "19px 0 0 0"}}>
+        </S.LogoBox>
+        <S.LogoBox Login>
+          <a style={{ margin: "19px 0 0 0" }}>
             <S.Text Blue>Login</S.Text>
           </a>
-          <a style={{marginTop: "7px"}}>
+          <a style={{ marginTop: "7px" }}>
             <S.Text Blue>Register</S.Text>
           </a>
-        </S.LogoTitleBox>
+        </S.LogoBox>
       </S.MainBox>
+      <S.Nav>
+        <a href="#">Main</a>
+        <a href="#">Notice</a>
+        <a href="#">Homework</a>
+        <a href="#">Fine</a>
+      </S.Nav>
+      <S.Container>
+        <S.DesignBox>
+          <div></div>
+          <div></div>
+          <div></div>
+        </S.DesignBox>
+      </S.Container>
     </S.Header>
   );
 };
--- src/component/Header/style.js
+++ src/component/Header/style.js
@@ -6,6 +6,9 @@ const Header = styled.div`
   width: 360.5px;
   background-color: #ffffff;
   border-right: solid #707070 1px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const MainBox = styled.div`
@@ -14,7 +17,7 @@ const MainBox = styled.div`
   margin: 194px 55px 0px 33px;
 `;
 
-const LogoTitleBox = styled.div`
+const LogoBox = styled.div`
   display: flex;
   width: 100%;
   justify-content: ${(props) => (props.Login ? "flex-start" : "flex-end")};
@@ -32,4 +35,58 @@ const Text = styled.p`
   color: ${(props) => (props.Blue ? "#3987EE" : "#393838")};
 `;
 
-export { Header, MainBox, Text, LogoTitleBox };
+const Nav = styled.div`
+  width: 174px;
+  height: 340px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 110px;
+  a:link {
+    text-decoration: none;
+  }
+  a:visited {
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: none;
+  }
+  a {
+    font-family: "고도 M";
+    font-size: 35px;
+    color: #393838;
+  }
+  a:nth-child(n + 2) {
+    margin-top: 60px;
+  }
+`;
+
+const DesignBox = styled.div`
+  width: 254px;
+  height: 64px;
+  div {
+    height: 33.3333333333333333333%;
+  }
+  div:nth-child(1) {
+    width: 50%;
+    background-color: #3987ee;
+  }
+  div:nth-child(2) {
+    width: 70%;
+    background-color: #3073cc;
+  }
+  div:nth-child(3) {
+    width: 100%;
+    background-color: #1862c2;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  width: 100%;
+  height: 100%;
+`;
+
+export { Header, MainBox, Text, LogoBox, Nav, DesignBox, Container };
