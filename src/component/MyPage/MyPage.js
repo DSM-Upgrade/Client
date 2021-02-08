5b433152433feda497a7b4a4d59f948bddc50c90
CutyApple <dbtldhs0915@naver.com>
2021-02-09 00:21:32 +0900

    [FEAT] <src/component/MyPage/MyPage.js>

--- src/component/MyPage/MyPage.js
+++ src/component/MyPage/MyPage.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { fineBackground } from "../../assets/fine";
+import * as S from "./style";
 
-const MyPage = () => {
-  return <div>Mypage</div>;
+const MyPage = (props) => {
+  const { data } = props;
+
+  return (
+    <S.Container img={fineBackground}>
+      {/* <S.BackgroundImg src={fineBackground} /> */}
+    </S.Container>
+  );
 };
 
 export default MyPage;
