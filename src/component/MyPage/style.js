1937907b6f772fb92ad64f241e5ada4633da9c0e
CutyApple <dbtldhs0915@naver.com>
2021-02-09 00:21:13 +0900

    [FEAT] <src/component/MyPage/style.js>

--- src/component/MyPage/style.js
+++ src/component/MyPage/style.js
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100%;
-  background: #f8fbff;
+  background: #f8fbff url(${(props) => props.img}) no-repeat 100% 100% /50% 50%;
 `;
