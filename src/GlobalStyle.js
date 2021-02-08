4aa1e19e23adb99ebad181f75f3eef30a10c87b5
CutyApple <dbtldhs0915@naver.com>
2021-02-09 00:35:54 +0900

    [FEAT] <src/GlobalStyle.js>

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+    html, body, #root {
+        height: 100%;
+    }
+
     html, body, div, span, applet, object, iframe,
     h1, h2, h3, h4, h5, h6, p, blockquote, pre,
     a, abbr, acronym, address, big, cite, code,
