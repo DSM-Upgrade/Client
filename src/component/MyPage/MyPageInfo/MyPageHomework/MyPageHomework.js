d361792232f9062b20e609a325a75655c67451e7
CutyApple <dbtldhs0915@naver.com>
2021-02-11 05:36:35 +0900

    [FEAT] <component/MyPage/~/MyPageHomework>

--- src/component/MyPage/MyPageInfo/MyPageHomework/MyPageHomework.js
+++ src/component/MyPage/MyPageInfo/MyPageHomework/MyPageHomework.js
@@ -2,7 +2,41 @@ import React from "react";
 import * as S from "./style";
 
 const MyPageHomework = () => {
-  return <S.Container></S.Container>;
+  const homeworks = [
+    {
+      homework_id: 1,
+      homework_title: "숙제 제목",
+      homework_start: "숙제 등록한 연-월-일",
+      homework_end: "숙제 기한 연-월-일",
+      homework_writer: "이름",
+    },
+    {
+      homework_id: 2,
+      homework_title: "숙제 제목",
+      homework_start: "숙제 등록한 연-월-일",
+      homework_end: "숙제 기한 연-월-일",
+      homework_writer: "이름",
+    },
+  ];
+  const cntOfHomeworks = homeworks.length;
+
+  return (
+    <S.Container>
+      <S.Title>현재 숙제</S.Title>
+      <S.SubTitle>
+        {cntOfHomeworks
+          ? `남은 숙제 수 : ${cntOfHomeworks}개`
+          : "숙제가 없네요"}
+      </S.SubTitle>
+      <S.HomeworkBox>
+        {homeworks.length
+          ? homeworks.map(({ homework_id: id, homework_title: title }) => (
+              <S.HomeworkItem key={id}>{title}</S.HomeworkItem>
+            ))
+          : ""}
+      </S.HomeworkBox>
+    </S.Container>
+  );
 };
 
 export default MyPageHomework;
