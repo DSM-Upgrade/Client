44fcce7ac1c23942f76d932b4cfbd52951731aba
CutyApple <dbtldhs0915@naver.com>
2021-02-11 05:23:10 +0900

    [FEAT] <container/~/MyPageFineContainer.js>

--- src/container/MyPageContanier/MyPageInfo/MyPageFine/MyPageFineContainer.js
+++ src/container/MyPageContanier/MyPageInfo/MyPageFine/MyPageFineContainer.js
@@ -2,7 +2,133 @@ import React from "react";
 import MyPageFine from "../../../../component/MyPage/MyPageInfo/MyPageFine/MyPageFine";
 
 const MyPageFineContainer = () => {
-  return <MyPageFine></MyPageFine>;
+  const fines = [
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+  ];
+  const sumOfFine = fines.length
+    ? fines.reduce((pre, cur) => pre + cur.fine, 0)
+    : 0;
+
+  return <MyPageFine fines={fines} sumOfFine={sumOfFine} />;
 };
 
 export default MyPageFineContainer;
