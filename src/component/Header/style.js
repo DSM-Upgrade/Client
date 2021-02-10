f1673d45957fb5e7335aba21f4359cb88e2f0ce4
Giljun Bae <bgj0127@gmail.com>
2021-02-11 05:31:38 +0900

    [FEAT]<component/Header/Header.js> Create File

---
+++ src/component/Header/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  position:relative;
+  width: 22.5rem;
+  height: 100vh;
+  background: #ffffff;
+`
