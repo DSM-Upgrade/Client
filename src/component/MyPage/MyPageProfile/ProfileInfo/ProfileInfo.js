14a8f9a7bd7106cbb849353cba7c3957bd14d2ef
CutyApple <dbtldhs0915@naver.com>
2021-02-10 21:54:23 +0900

    [FEAT] <component/~/ProfileInfo>

--- src/component/MyPage/MyPageProfile/ProfileInfo/ProfileInfo.js
+++ src/component/MyPage/MyPageProfile/ProfileInfo/ProfileInfo.js
@@ -1,8 +1,33 @@
 import React from "react";
 import * as S from "./style";
 
-const ProfileInfo = () => {
-  return <S.Container></S.Container>;
+const ProfileInfo = (props) => {
+  const { name, student_num, field, username } = props;
+
+  return (
+    <S.Container>
+      <S.InfoWrap>
+        <S.InfoName>이름</S.InfoName>
+        <S.InfoValue type="text" value={name} />
+      </S.InfoWrap>
+      <S.InfoWrap>
+        <S.InfoName>학번</S.InfoName>
+        <S.InfoValue type="number" value={student_num} />
+      </S.InfoWrap>
+      <S.InfoWrap>
+        <S.InfoName>아이디</S.InfoName>
+        <S.InfoValue type="text" value={username} />
+      </S.InfoWrap>
+      <S.InfoWrap>
+        <S.InfoName>비밀번호</S.InfoName>
+        <S.InfoValue />
+      </S.InfoWrap>
+      <S.InfoWrap>
+        <S.InfoName>전공</S.InfoName>
+        <S.InfoValue type="text" value={field} />
+      </S.InfoWrap>
+    </S.Container>
+  );
 };
 
 export default ProfileInfo;
--- src/component/MyPage/MyPageProfile/ProfileInfo/style.js
+++ src/component/MyPage/MyPageProfile/ProfileInfo/style.js
@@ -4,3 +4,18 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
 `;
+
+export const InfoWrap = styled.div``;
+
+export const InfoName = styled.p`
+  color: #000000;
+  font-size: 24px;
+`;
+
+export const InfoValue = styled.input`
+  color: #000000;
+  font-size: 24px;
+  outline: none;
+  border: 1px solid #eef0f2;
+  border-radius: 8px;
+`;
