e5ff89a6c02c786733afde59e26259a9d0e72e4e
CutyApple <dbtldhs0915@naver.com>
2021-02-10 17:38:48 +0900

    [FEAT] <component/MyPage>

--- src/component/MyPage/MyPage.js
+++ src/component/MyPage/MyPage.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { fineBackground } from "../../assets/fine";
+import MyPageFine from "./MyPageFine/MyPageFine";
+import MyPageProfile from "./MyPageProfile/MyPageProfile";
 import * as S from "./style";
 
 const MyPage = (props) => {
   const { data } = props;
 
   return (
-    <S.Container img={fineBackground}>
-      {/* <S.BackgroundImg src={fineBackground} /> */}
+    <S.Container>
+      <S.BackgroundImg img={fineBackground}>
+        <S.Wrapper>
+          <MyPageProfile />
+          <MyPageFine />
+        </S.Wrapper>
+      </S.BackgroundImg>
     </S.Container>
   );
 };
--- src/component/MyPage/style.js
+++ src/component/MyPage/style.js
@@ -3,5 +3,17 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100%;
+`;
+
+export const BackgroundImg = styled.div`
+  width: 100%;
+  height: 100%;
   background: #f8fbff url(${(props) => props.img}) no-repeat 100% 100% /50% 50%;
+`;
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 7fr 3fr;
+  width: 100%;
+  height: 100%;
 `;
