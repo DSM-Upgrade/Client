795901741ea763d5b54af0c713e0e0f864c4ae77
CutyApple <dbtldhs0915@naver.com>
2021-02-10 16:42:52 +0900

    [TEST] <__test__/MyPage/MyPage.test.js>

---
+++ src/__test__/MyPage/MyPage.test.js
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import MyPage from "../../component/MyPage/MyPage";
+
+describe("<MyPage />", () => {
+  it("", () => {
+    const {} = render(<MyPage />);
+  });
+});
