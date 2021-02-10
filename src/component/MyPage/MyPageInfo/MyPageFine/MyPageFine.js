ef4a5a3c66a2c738535d1a9e84ab24206da60c3d
CutyApple <dbtldhs0915@naver.com>
2021-02-11 05:36:52 +0900

    [FEAT] <component/MyPage/~/MyPageFine>

--- src/component/MyPage/MyPageInfo/MyPageFine/MyPageFine.js
+++ src/component/MyPage/MyPageInfo/MyPageFine/MyPageFine.js
@@ -2,7 +2,151 @@ import React from "react";
 import * as S from "./style";
 
 const MyPageFine = () => {
-  return <S.Container></S.Container>;
+  const fines = [
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+    {
+      fine_id: 1,
+      fine_reason: "지각",
+      fine_date: "2021-1-26",
+      fine: 3000,
+    },
+    {
+      fine_id: 2,
+      fine_reason: "꿀잠",
+      fine_date: "2021-2-11",
+      fine: 2000,
+    },
+  ];
+  const sumOfFine = fines.length
+    ? fines.reduce((pre, cur) => pre + cur.fine, 0)
+    : 0;
+
+  return (
+    <S.Container>
+      <S.Title>미납 벌금</S.Title>
+      <S.SubTitle>
+        {sumOfFine ? `총합 ${sumOfFine}원` : `낼 벌금이 없으시네요!`}
+      </S.SubTitle>
+      <S.FineBox>
+        {fines.length
+          ? fines.map(
+              ({ fine_id: id, fine_reason: reason, fine_date: date, fine }) => (
+                <S.FineItem
+                  key={id}
+                >{`${date} - ${reason} - ${fine}`}</S.FineItem>
+              )
+            )
+          : ""}
+      </S.FineBox>
+    </S.Container>
+  );
 };
 
 export default MyPageFine;
--- src/component/MyPage/MyPageInfo/MyPageFine/style.js
+++ src/component/MyPage/MyPageInfo/MyPageFine/style.js
@@ -1,3 +1,39 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: grid;
+  grid-template-rows: 30px 30px 1fr;
+`;
+
+export const Title = styled.p`
+  color: #000000;
+  font-size: 18px;
+  text-align: center;
+`;
+
+export const SubTitle = styled(Title)`
+  font-size: 16px;
+`;
+
+export const FineBox = styled.div`
+  width: 100%;
+  height: 300px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+  &::-webkit-scrollbar-track {
+    background: none;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #707070;
+    border-radius: 6px;
+  }
+`;
+
+export const FineItem = styled.div`
+  width: 100%;
+  margin: 3px;
+`;
