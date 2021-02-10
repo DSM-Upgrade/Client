ebefeb2fe4fce5fc81ba39db03b44945e3d115a1
CutyApple <dbtldhs0915@naver.com>
2021-02-11 04:31:52 +0900

    [FEAT] <component/~/MyPageProfile/style.js>

--- src/component/MyPage/MyPageProfile/style.js
+++ src/component/MyPage/MyPageProfile/style.js
@@ -4,6 +4,6 @@ export const Container = styled.div`
   display: grid;
   grid-template-columns: 3fr 7fr;
   width: 100%;
-  height: calc(100% - 120px);
-  padding: 120px 70px 0 70px;
+  height: calc(100% - 240px);
+  padding: 120px 70px 120px 70px;
 `;
