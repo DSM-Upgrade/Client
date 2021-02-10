a6254f7fc426fd78980b4a158e1f186edbd721b1
CutyApple <dbtldhs0915@naver.com>
2021-02-11 05:42:45 +0900

    [FEAT] <container/~/MyPageFineContainer.js> Create File

---
+++ src/container/MyPageContanier/MyPageInfo/MyPageFine/MyPageFineContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MyPageFine from "../../../../component/MyPage/MyPageInfo/MyPageFine/MyPageFine";
+
+const MyPageFineContainer = () => {
+  return <MyPageFine></MyPageFine>;
+};
+
+export default MyPageFineContainer;
