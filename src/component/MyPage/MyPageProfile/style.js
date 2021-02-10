d449a677d313c839c10d4b7c7b97edb57bf6e396
CutyApple <dbtldhs0915@naver.com>
2021-02-10 22:07:21 +0900

    [FIX] <component/~.MyPageProfile/style.js> Set Layout

--- src/component/MyPage/MyPageProfile/style.js
+++ src/component/MyPage/MyPageProfile/style.js
@@ -4,6 +4,6 @@ export const Container = styled.div`
   display: grid;
   grid-template-columns: 3fr 7fr;
   width: 100%;
-  height: 100%;
+  height: calc(100% - 120px);
   padding: 120px 70px 0 70px;
 `;
