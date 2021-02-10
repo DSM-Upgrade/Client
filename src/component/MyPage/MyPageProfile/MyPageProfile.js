f227b7d86e5a23e5f1b2b9aa8064a795f1ccf250
CutyApple <dbtldhs0915@naver.com>
2021-02-10 16:46:56 +0900

    [FEAT] <component/~/MyPageProfile/MyPageProfile.js> Apply Child

--- src/component/MyPage/MyPageProfile/MyPageProfile.js
+++ src/component/MyPage/MyPageProfile/MyPageProfile.js
@@ -1,8 +1,15 @@
 import React from "react";
+import ProfileImg from "./ProfileImg/ProfileImg";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import * as S from "./style";
 
 const Profile = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <ProfileImg />
+      <ProfileInfo />
+    </S.Container>
+  );
 };
 
 export default Profile;
