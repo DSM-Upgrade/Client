19b238f7a0da5809366fe35198abd19f71988df4
CutyApple <dbtldhs0915@naver.com>
2021-02-10 21:54:08 +0900

    [FEAT] <container/~/ProfileInfoContainer.js> Add TempData

--- src/container/MyPageContanier/MyPageProfile/ProfileInfo/ProfileInfoContainer.js
+++ src/container/MyPageContanier/MyPageProfile/ProfileInfo/ProfileInfoContainer.js
@@ -2,7 +2,14 @@ import React from "react";
 import ProfileInfo from "../../../../component/MyPage/MyPageProfile/ProfileInfo/ProfileInfo";
 
 const MyPageInfoContainer = () => {
-  return <ProfileInfo />;
+  const TEMP_DATA = {
+    name: "유시온",
+    student_num: "3415",
+    field: "프론트엔드",
+    username: "cutyapple123",
+  };
+
+  return <ProfileInfo {...TEMP_DATA} />;
 };
 
 export default MyPageInfoContainer;
