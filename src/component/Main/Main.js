40fe37d05b006f90802b296b57191f371e401ee5
Giljun Bae <bgj0127@gmail.com>
2021-02-11 05:30:24 +0900

    [FEAT]<component/Main/Main.js> Create File

---
+++ src/component/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './style';
+import Header from "../Header/Header";
+
+const Main = () => {
+  return (
+    <S.Container>
+      <Header />
+    </S.Container>
+  );
+};
+
+export default Main;
