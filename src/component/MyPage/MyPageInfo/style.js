c0c33756875fcf7381ca7f83718b7bfd40cd13d9
CutyApple <dbtldhs0915@naver.com>
2021-02-11 05:36:17 +0900

    [FIX] <component/MyPage/MyPageInfo/style.js> Modify Layout

--- src/component/MyPage/MyPageInfo/style.js
+++ src/component/MyPage/MyPageInfo/style.js
@@ -1,14 +1,19 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  display: grid;
   width: 100%;
   height: calc(100% - 240px);
   padding: 120px 0;
 `;
 
 export const InfoBackground = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 10px;
   width: 400px;
-  height: 400px;
+  height: 360px;
+  padding: 20px 10px;
   border: none;
   border-radius: 16px;
   background: #ffffff;
