9e0417e3f35ed1e5dd02490aea2b000067221a76
CutyApple <dbtldhs0915@naver.com>
2021-02-12 07:35:19 +0900

    [FEAT] <src/Routing.js> Apply ModalWrapperContainer

--- src/Routing.js
+++ src/Routing.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import ModalWrapperContainer from "./container/ModalWrapperContainer/ModalWrapperContainer";
 import MyPageContanier from "./container/MyPageContanier/MyPageContanier";
 
 const Routing = () => {
@@ -8,6 +9,7 @@ const Routing = () => {
       <Switch>
         <Route exact path="/mypage" component={MyPageContanier} />
       </Switch>
+      <ModalWrapperContainer />
     </>
   );
 };
