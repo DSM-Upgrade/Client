83f1afaea32357ef1aee63a768b54729eaa40a4e
CutyApple <dbtldhs0915@naver.com>
2021-02-12 01:14:55 +0900

    [FIX] <component/MyPage/MyPageProfile/style.js> Modify Layout

--- src/component/MyPage/MyPageProfile/style.js
+++ src/component/MyPage/MyPageProfile/style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: grid;
   grid-template-columns: 3fr 7fr;
-  width: 100%;
-  height: calc(100% - 240px);
-  padding: 120px 70px 120px 70px;
+  width: calc(100% - 140px);
+  height: calc(100% - 360px);
+  padding: 180px 70px 180px 70px;
 `;
