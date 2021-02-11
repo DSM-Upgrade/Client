97eed829ef7166aef695f7ea79b929c31c354b6c
CutyApple <dbtldhs0915@naver.com>
2021-02-12 01:09:15 +0900

    [FEAT] <container/~/ProfileInfoContainer.js>

--- src/container/MyPageContanier/MyPageProfile/ProfileInfo/ProfileInfoContainer.js
+++ src/container/MyPageContanier/MyPageProfile/ProfileInfo/ProfileInfoContainer.js
@@ -5,17 +5,52 @@ const MyPageInfoContainer = () => {
   const TEMP_DATA = {
     name: "유시온",
     student_num: "3415",
-    field: "프론트엔드",
+    field: "웹 프론트엔드",
     username: "cutyapple123",
   };
+  const FieldsData = [
+    {
+      id: 1,
+      name: "ios",
+    },
+    {
+      id: 2,
+      name: "디자인",
+    },
+    {
+      id: 3,
+      name: "웹 백엔드",
+    },
+    {
+      id: 4,
+      name: "안드로이드",
+    },
+    {
+      id: 5,
+      name: "웹 프론트엔드",
+    },
+  ];
 
   const [studentNum, setStudentNum] = useState(TEMP_DATA.student_num);
+  const [field, setField] = useState(TEMP_DATA.field);
+
   const onChangeStdNum = useCallback((e) => {
     setStudentNum(e.target.value);
   }, []);
   const studentNumManage = { studentNum, onChangeStdNum };
 
-  return <ProfileInfo {...TEMP_DATA} studentNumManage={studentNumManage} />;
+  const onChangeField = useCallback((e) => {
+    setField(e.target.value);
+  });
+  const fieldManage = { field, FieldsData, onChangeField };
+
+  return (
+    <ProfileInfo
+      {...TEMP_DATA}
+      studentNumManage={studentNumManage}
+      fieldManage={fieldManage}
+    />
+  );
 };
 
 export default MyPageInfoContainer;
