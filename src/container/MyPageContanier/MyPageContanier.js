134dae8893e050125b7cd4963a56364713110823
CutyApple <dbtldhs0915@naver.com>
2021-02-12 00:57:59 +0900

    [FIX] <container/~/MyPageContainer.js> Delete Temp Wrap

--- src/container/MyPageContanier/MyPageContanier.js
+++ src/container/MyPageContanier/MyPageContanier.js
@@ -2,18 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import Mypage from "../../component/MyPage/MyPage";
 
-const TempWrap = styled.div`
-  margin-left: 20%;
-  width: 80%;
-  height: 100%;
-`;
-
 const MyPageContanier = () => {
-  return (
-    <TempWrap>
-      <Mypage />
-    </TempWrap>
-  );
+  return <Mypage />;
 };
 
 export default MyPageContanier;
