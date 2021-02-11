d60063824a0d9384d893a1e51ac558571c29c738
Giljun Bae <bgj0127@gmail.com>
2021-02-11 22:38:09 +0900

    [FEAT]<component/~/Main.js>

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import * as S from './style';
+import { mainBackground } from '../../assets/main';
 import Header from "../Header/Header";
+import Footer from "./Footer/Footer";
 
 const Main = () => {
   return (
-    <S.Container>
+    <S.Main>
       <Header />
-    </S.Container>
+      <S.Container>
+          <S.Inner>
+            <S.BackgroundImg img={mainBackground}></S.BackgroundImg>
+            <S.Glass></S.Glass>
+          </S.Inner>
+          <Footer />
+      </S.Container>
+    </S.Main>
   );
 };
 
--- src/component/Main/style.js
+++ src/component/Main/style.js
@@ -1,7 +1,35 @@
 import styled from 'styled-components'  
 
+export const Main = styled.div`
+  display: flex;
+`
+
 export const Container = styled.div`
-  width: 100%;
+  position: relative;
+  width: 97.5rem;
   height :100vh;
   background: #F8FBFF;
+`
+
+export const Inner = styled.div`
+  position: relative;
+  height: 70%;
+  background: #F8FBFF;
+`
+
+export const BackgroundImg = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #f8fbff url(${(props) => props.img}) no-repeat 50% 80%;
+`;
+
+export const Glass = styled.div`
+  position: absolute;
+  width: 94rem;
+  height: 40rem;
+  background: #AEB9D4;
+  opacity: 0.12;
+  top:0;
+  margin: 32px 26px;
+  border-radius: 12px;
 `
