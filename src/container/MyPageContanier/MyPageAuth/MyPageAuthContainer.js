8e66b94e00574af5ce486e1e275d5ded9abe452c
CutyApple <dbtldhs0915@naver.com>
2021-02-12 11:50:17 +0900

    [FEAT] <container/~/MyPageAuthContainer.js> Create File

---
+++ src/container/MyPageContanier/MyPageAuth/MyPageAuthContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MyPageAuth from "../../../component/MyPage/MyPageAuth/MyPageAuth";
+
+const MyPageAuthContainer = () => {
+  return <MyPageAuth />;
+};
+
+export default MyPageAuthContainer;
