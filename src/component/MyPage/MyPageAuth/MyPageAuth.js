6b366eb9cef36d38fb97b212e7484b37e532f008
CutyApple <dbtldhs0915@naver.com>
2021-02-12 11:30:04 +0900

    [FEAT] <component/~/MyPageAuth>

--- src/component/MyPage/MyPageAuth/MyPageAuth.js
+++ src/component/MyPage/MyPageAuth/MyPageAuth.js
@@ -2,7 +2,11 @@ import React from "react";
 import * as S from "./style";
 
 const MyPageAuth = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <S.InfoBackground></S.InfoBackground>
+    </S.Container>
+  );
 };
 
 export default MyPageAuth;
