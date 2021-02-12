768960f18a97427700329a58a9dc8a9fe679c061
CutyApple <dbtldhs0915@naver.com>
2021-02-12 11:43:14 +0900

    [FEAT] <container/~/MyPageContainer.js>

--- src/container/MyPageContanier/MyPageContanier.js
+++ src/container/MyPageContanier/MyPageContanier.js
@@ -1,9 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import { fineBackground } from "../../assets/fine";
 import Mypage from "../../component/MyPage/MyPage";
 
 const MyPageContanier = () => {
-  return <Mypage />;
+  const img = fineBackground;
+  const isAdmin = true;
+
+  return <Mypage img={img} isAdmin={isAdmin} />;
 };
 
 export default MyPageContanier;
