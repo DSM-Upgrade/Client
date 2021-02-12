3b32e410559660f90d045ea09d9e2018d35023e7
CutyApple <dbtldhs0915@naver.com>
2021-02-12 11:55:05 +0900

    [FEAT] <container/~/MyPageAuthContainer.js>

--- src/container/MyPageContanier/MyPageAuth/MyPageAuthContainer.js
+++ src/container/MyPageContanier/MyPageAuth/MyPageAuthContainer.js
@@ -2,7 +2,30 @@ import React from "react";
 import MyPageAuth from "../../../component/MyPage/MyPageAuth/MyPageAuth";
 
 const MyPageAuthContainer = () => {
-  return <MyPageAuth />;
+  const authList = [
+    {
+      stdNum: "3415",
+      name: "유시온",
+      id: "1",
+    },
+    {
+      stdNum: "3415",
+      name: "유시온2",
+      id: "2",
+    },
+    {
+      stdNum: "3415",
+      name: "유시온3",
+      id: "3",
+    },
+    {
+      stdNum: "3415",
+      name: "유시온4",
+      id: "4",
+    },
+  ];
+
+  return <MyPageAuth authList={authList} />;
 };
 
 export default MyPageAuthContainer;
