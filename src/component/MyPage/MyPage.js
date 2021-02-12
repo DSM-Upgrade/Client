2831c5b7894fbd7ce27a8c5c9e06f6c6f3e42b50
CutyApple <dbtldhs0915@naver.com>
2021-02-12 11:42:58 +0900

    [FEAT] <component/MyPage/MyPage.js> Apply Container Props

--- src/component/MyPage/MyPage.js
+++ src/component/MyPage/MyPage.js
@@ -1,18 +1,20 @@
 import React from "react";
 import * as S from "./style";
-import { fineBackground } from "../../assets/fine";
 import MyPageInfo from "./MyPageInfo/MyPageInfo";
 import MyPageProfile from "./MyPageProfile/MyPageProfile";
 import HeaderContainer from "../../container/HeaderContainer/HeaderContainer";
+import MyPageAuth from "./MyPageAuth/MyPageAuth";
 
-const MyPage = () => {
+const MyPage = (props) => {
+  const { img, isAdmin } = props;
+
   return (
     <S.Container>
       <HeaderContainer />
-      <S.BackgroundImg img={fineBackground}>
+      <S.BackgroundImg img={img}>
         <S.Wrapper>
           <MyPageProfile />
-          <MyPageInfo />
+          {isAdmin ? <MyPageAuth /> : <MyPageInfo />}
         </S.Wrapper>
       </S.BackgroundImg>
     </S.Container>
