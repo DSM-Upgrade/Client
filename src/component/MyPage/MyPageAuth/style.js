b061baf466595da3cbed00ce6848fdb992f9b182
CutyApple <dbtldhs0915@naver.com>
2021-02-12 11:55:30 +0900

    [FEAT] <component/MyPage/MyPageAuth>

--- src/component/MyPage/MyPageAuth/style.js
+++ src/component/MyPage/MyPageAuth/style.js
@@ -9,13 +9,63 @@ export const Container = styled.div`
 `;
 
 export const InfoBackground = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 10px;
-  width: 400px;
+  display: flex;
+  flex-direction: column;
+  width: 380px;
   height: 360px;
-  padding: 20px 10px;
+  padding: 20px 20px;
   border: 1px solid #eef0f2;
   border-radius: 16px;
   background: #ffffff;
+`;
+
+export const Title = styled.p`
+  color: #000000;
+  font-size: 24px;
+  margin: 0 0 10px 10px;
+`;
+
+export const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  background: #bad7ff;
+`;
+
+export const AuthBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 5px 0 0 0;
+`;
+
+export const AuthItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 24px;
+  padding: 3px 3px;
+`;
+
+export const AuthInfo = styled.p`
+  color: #000000;
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+export const ButtonWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 106px;
+`;
+
+export const AuthButton = styled.button`
+  width: 50px;
+  height: 24px;
+  color: #ffffff;
+  border: none;
+  border-radius: 4px;
+  outline: none;
+  background: ${(props) => props.background || "#9FBCFF"};
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
