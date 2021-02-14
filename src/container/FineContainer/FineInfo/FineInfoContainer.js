b479018e287c8047ec3149ee2a65b3022339b9fd
CutyApple <dbtldhs0915@naver.com>
2021-02-15 06:01:18 +0900

    [FEAT] <container/~/FineInfoContainer.js> Temp fineData

--- src/container/FineContainer/FineInfo/FineInfoContainer.js
+++ src/container/FineContainer/FineInfo/FineInfoContainer.js
@@ -9,7 +9,30 @@ const FineInfoContainer = () => {
     field: "이것두..",
   };
 
-  return <FineInfo name={userData.name} student_num={userData.student_num} />;
+  const fineData = [
+    {
+      name: "고도현",
+      reason: "회의 미참여 실화..?",
+      price: "10000",
+      date: "2021-02-15",
+      isPay: false,
+    },
+    {
+      name: "유시온",
+      reason: "3학년이 회의 좀 늦을수도 있지..안그렇니!!!!!!!!!!!!",
+      price: "1000",
+      date: "2021-02-15",
+      isPay: true,
+    },
+  ];
+
+  return (
+    <FineInfo
+      name={userData.name}
+      student_num={userData.student_num}
+      fineData={fineData}
+    />
+  );
 };
 
 export default FineInfoContainer;
