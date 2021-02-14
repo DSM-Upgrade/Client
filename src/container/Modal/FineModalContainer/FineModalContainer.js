d7a20a31b53872c4a4c004ba0720fd790ac17cb6
CutyApple <dbtldhs0915@naver.com>
2021-02-15 06:54:59 +0900

    [FEAT] <container/~/FineModalContainer.js> Apply fineData

--- src/container/Modal/FineModalContainer/FineModalContainer.js
+++ src/container/Modal/FineModalContainer/FineModalContainer.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useCallback } from "react";
 import FineModal from "../../../component/Modal/FineModal/FineModal";
 
 const FineModalContainer = () => {
-  return <FineModal />;
+  const fineData = [
+    {
+      reason: "회의 미참여 실화..?",
+      price: "10000",
+      date: "2021-02-15",
+    },
+    {
+      reason: "3학년이 회의 좀 늦을수도 있지..안그렇니!!!!!!!!!!!!",
+      price: "1000",
+      date: "2021-02-15",
+    },
+  ];
+
+  const PreventModalOff = useCallback((e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }, []);
+
+  return <FineModal fineData={fineData} PreventModalOff={PreventModalOff} />;
 };
 
 export default FineModalContainer;
