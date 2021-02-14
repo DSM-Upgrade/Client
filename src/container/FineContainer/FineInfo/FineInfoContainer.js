0a12c7737926aded4f163612969d42bb7c51a2a6
CutyApple <dbtldhs0915@naver.com>
2021-02-15 03:56:04 +0900

    [FEAT] <container/~/FineInfoContainer.js> Apply FinInfo to Temp userData

---
+++ src/container/FineContainer/FineInfo/FineInfoContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import FineInfo from "../../../component/Fine/FineInfo/FineInfo";
+
+const FineInfoContainer = () => {
+  const userData = {
+    name: "유시온",
+    student_num: "3415",
+    username: "사실필요없음",
+    field: "이것두..",
+  };
+
+  return <FineInfo name={userData.name} />;
+};
+
+export default FineInfoContainer;
