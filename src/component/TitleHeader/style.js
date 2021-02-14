c737072cc1c82293e1d6f4e7ad26982d89d6e9c6
CutyApple <dbtldhs0915@naver.com>
2021-02-15 03:47:44 +0900

    [FEAT] <component/TitleHeader/style.js> Set Layout

--- src/component/TitleHeader/style.js
+++ src/component/TitleHeader/style.js
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: min-content;
+  padding: 0 0 30px 0;
+  border-bottom: 1px solid #000000;
+`;
 
 export const FirstChar = styled.p`
   color: #3987ee;
