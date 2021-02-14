8182364579e3c276fea0f4ab97b985a6a18e5d1c
CutyApple <dbtldhs0915@naver.com>
2021-02-15 06:09:45 +0900

    [FEAT] <container/~/FineModalContainer.js> FineModal

---
+++ src/container/Modal/FineModalContainer/FineModalContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import FineModal from "../../../component/Modal/FineModal/FineModal";
+
+const FineModalContainer = () => {
+  return <FineModal />;
+};
+
+export default FineModalContainer;
