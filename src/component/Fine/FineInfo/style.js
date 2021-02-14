b30ede220f8f9b0406d06812b466ec1c8714d1b4
CutyApple <dbtldhs0915@naver.com>
2021-02-15 04:18:03 +0900

    [FEAT] <component/Fine/FineInfo/style.js> Set Style

--- src/component/Fine/FineInfo/style.js
+++ src/component/Fine/FineInfo/style.js
@@ -1,8 +1,34 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: grid;
+  grid-template-rows: 70px 60px 1fr;
+  height: 100%;
+`;
 
 export const NameTitle = styled.p`
-  color: #000000;
+  line-height: 70px;
+  color: #393838;
   font-size: 40px;
+`;
+
+export const FineSummary = styled.div``;
+
+export const TextWrap = styled.div`
+  display: flex;
+`;
+
+export const GeneralText = styled.p`
+  color: #393838;
+  font-size: 20px;
+`;
+
+export const ColorText = styled.p`
+  color: #3987ee;
+  font-size: 20px;
+  padding: 0 0 0 5px;
+`;
+
+export const FineBox = styled.div`
+  background: #eef5ff;
 `;
