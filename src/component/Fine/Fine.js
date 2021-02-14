3409ae78ba76aadd6179e9beea8aa585e477d3a4
CutyApple <dbtldhs0915@naver.com>
2021-02-15 03:33:31 +0900

    [FEAT] <compnent/Fine> Apply fineBackground

--- src/component/Fine/Fine.js
+++ src/component/Fine/Fine.js
@@ -2,10 +2,15 @@ import React from "react";
 import * as S from "./style";
 import HeaderContainer from "../../container/HeaderContainer/HeaderContainer";
 
-const Fine = () => {
+const Fine = (props) => {
+  const { img } = props;
+
   return (
     <S.Container>
       <HeaderContainer />
+      <S.BackgroundImg img={img}>
+        <S.Wrapper></S.Wrapper>
+      </S.BackgroundImg>
     </S.Container>
   );
 };
--- src/component/Fine/style.js
+++ src/component/Fine/style.js
@@ -5,3 +5,16 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
 `;
+
+export const BackgroundImg = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: #f8fbff url(${(props) => props.img}) no-repeat 90% 10% / 20%;
+`;
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 7fr 3fr;
+  width: 100%;
+  height: 100%;
+`;
