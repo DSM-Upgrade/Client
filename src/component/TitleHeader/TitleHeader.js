520cb306a3e5b2a474cb402ca09a802b7d84b03a
CutyApple <dbtldhs0915@naver.com>
2021-02-15 03:41:31 +0900

    [FEAT] <component/TitleHeader> Complete

--- src/component/TitleHeader/TitleHeader.js
+++ src/component/TitleHeader/TitleHeader.js
@@ -1,8 +1,15 @@
 import React from "react";
 import * as S from "./style";
 
-const TitleHeader = () => {
-  return <S.Container></S.Container>;
+const TitleHeader = (props) => {
+  const { firstChar, otherChar } = props;
+
+  return (
+    <S.Container>
+      <S.FirstChar>{firstChar}</S.FirstChar>
+      <S.OtherChar>{otherChar}</S.OtherChar>
+    </S.Container>
+  );
 };
 
 export default TitleHeader;
--- src/component/TitleHeader/style.js
+++ src/component/TitleHeader/style.js
@@ -1,3 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
+
+export const FirstChar = styled.p`
+  color: #3987ee;
+  font-size: 44px;
+`;
+
+export const OtherChar = styled.p`
+  color: #393838;
+  font-size: 44px;
+`;
