592d56c21d9496d83242691edc3d6b7370471d14
Giljun Bae <bgj0127@gmail.com>
2021-02-16 21:08:41 +0900

    [FEAT]<assets/main/index.js>Apply Arrow png

--- src/assets/main/index.js
+++ src/assets/main/index.js
@@ -6,3 +6,6 @@ export { default as mainSlideNotice } from './mainSlideNotice.png'
 export { default as mainSlideHomework } from './mainSlideHomework.png'
 export { default as mainSlideFine } from './mainSlideFine.png'
 export { default as mainSlideMypage } from './mainSlideMypage.png'
+export { default as leftArrow } from './leftArrow.png'
+export { default as rightArrow } from './rightArrow.png'
+
