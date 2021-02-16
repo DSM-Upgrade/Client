90bb315a04518696546d75bc7aab604cefc628e4
CutyApple <dbtldhs0915@naver.com>
2021-02-16 22:30:28 +0900

    [FEAT] <component/Fine/FineAdmin>

--- src/component/Fine/FineAdmin/FineAdmin.js
+++ src/component/Fine/FineAdmin/FineAdmin.js
@@ -1,8 +1,48 @@
 import React from "react";
 import * as S from "./style";
 
-const FineAdmin = () => {
-  return <S.Container></S.Container>;
+const FineAdmin = (props) => {
+  const { fineData } = props;
+
+  return (
+    <S.Container>
+      <S.NameTitle>벌금 추가</S.NameTitle>
+      <S.AddFine>
+        <S.TempGridWrap>
+          <S.TempFlexWrap>
+            <S.AddFineLabel htmlFor="add_fine_input_name">대상</S.AddFineLabel>
+            <S.AddFineInput type="text" id="add_fine_input_name" />
+          </S.TempFlexWrap>
+          <S.TempFlexWrap>
+            <S.AddFineLabel htmlFor="add_fine_input_price">금액</S.AddFineLabel>
+            <S.AddFineInput type="text" id="add_fine_input_price" />
+          </S.TempFlexWrap>
+          <S.TempFlexWrap>
+            <S.AddFineLabel htmlFor="add_fine_input_date">날짜</S.AddFineLabel>
+            <S.AddFineInput type="date" id="add_fine_input_date" />
+          </S.TempFlexWrap>
+        </S.TempGridWrap>
+        <S.TempFlexWrap>
+          <S.AddFineLabel htmlFor="add_fine_input_text">사유</S.AddFineLabel>
+          <S.AddFineInput type="text" id="add_fine_input_text" />
+        </S.TempFlexWrap>
+      </S.AddFine>
+      <S.FineBox>
+        {fineData.map((fine, index) => {
+          const { name, reason, price, date } = fine;
+          return (
+            <S.FineItem key={index}>
+              <S.ItemText>{name}</S.ItemText>
+              <S.ItemText>{reason}</S.ItemText>
+              <S.ItemText textAlign="right">{price}원</S.ItemText>
+              <S.ItemText textAlign="center">{date}</S.ItemText>
+              <S.ItemButton>입금확인</S.ItemButton>
+            </S.FineItem>
+          );
+        })}
+      </S.FineBox>
+    </S.Container>
+  );
 };
 
 export default FineAdmin;
