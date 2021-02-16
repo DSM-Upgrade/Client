55cfa865a0b38b0649d880cde003dc6ecea88ae7
Giljun Bae <bgj0127@gmail.com>
2021-02-16 18:39:41 +0900

    [FEAT]<assets/main/index.js> Apply Main Slide png

--- src/assets/main/index.js
+++ src/assets/main/index.js
@@ -1,3 +1,8 @@
 export { default as mainBackground } from "./mainBackgroundImg.png"
 export { default as iconFacebook } from './iconFacebook.png'
 export { default as iconGithub } from './iconGithub.png'
+export { default as mainSlide } from './mainSlide.png'
+export { default as mainSlideNotice } from './mainSlideNotice.png'
+export { default as mainSlideHomework } from './mainSlideHomework.png'
+export { default as mainSlideFine } from './mainSlideFine.png'
+export { default as mainSlideMypage } from './mainSlideMypage.png'
