ec176a0afaac1e05d84a17bdff882f96c354016b
JaeHyun <soehd0889@naver.com>
2021-02-17 22:37:18 +0900

    [FEAT] <src/App.js> Test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./GlobalStyle";
 import Routing from "./Routing";
-import RegisterPage from './component/RegisterPage/RegisterPage'
+import RegisterPage from './component/LoginPage/LoginPage'
+import LoginPage from "./component/LoginPage/LoginPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routing />
-      <RegisterPage />
+      <LoginPage />
     </BrowserRouter>
   );
 };
