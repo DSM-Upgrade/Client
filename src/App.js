3124914bc538625939a1a908388bc4dc656452a5
JaeHyun <soehd0889@naver.com>
2021-02-18 11:50:35 +0900

    [FEAT] <src/component/Homework> Delete File

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./GlobalStyle";
 import Routing from "./Routing";
-import Homework from "./component/Homework/Homework";
 
 const App = () => {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routing />
-      <Homework />
     </BrowserRouter>
   );
 };
