508912c6ed2996c20696807d2e0c2781bf5d930d
JaeHyun <soehd0889@naver.com>
2021-02-18 11:46:13 +0900

    [FEAT] <src/component/Homework> Create File

---
+++ src/component/Homework/Homework.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Header/Header"
+
+const Homework = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+export default Homework;
