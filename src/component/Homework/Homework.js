9e4fcf8dd663e2b33f0bbab55afc873b7b8ad3f4
JaeHyun <soehd0889@naver.com>
2021-02-19 15:20:56 +0900

    [FEAT] <src/component/Homework> Set Header

---
+++ src/component/Homework/Homework.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HeaderContainer from "../../container/HeaderContainer/HeaderContainer";
+import * as S from "./style";
+
+const Homework = () => {
+  return (
+    <div>
+      <HeaderContainer />
+    </div>
+  );
+};
+
+export default Homework;
