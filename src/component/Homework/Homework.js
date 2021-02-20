1fe0498f652e5739f6540b67c8054a4c832b412a
JaeHyun <soehd0889@naver.com>
2021-02-20 23:59:41 +0900

    [FEAT] <component/~/Homework.js, style.js>

--- src/component/Homework/Homework.js
+++ src/component/Homework/Homework.js
@@ -1,13 +1,17 @@
 import React from "react";
-import HeaderContainer from "../../container/HeaderContainer/HeaderContainer";
 import * as S from "./style";
+import HeaderContainer from "../../container/HeaderContainer/HeaderContainer";
+import TitleHeaderContainer from "../../container/TitleHeaderContainer/TitleHeaderContainer";
 
 const Homework = () => {
   return (
     <S.Container>
       <HeaderContainer />
       <S.MainWrapper>
-        
+        <TitleHeaderContainer text="Homework" />
+        <S.Wrapper>
+          
+        </S.Wrapper>
       </S.MainWrapper>
     </S.Container>
   );
--- src/component/Homework/style.js
+++ src/component/Homework/style.js
@@ -3,12 +3,26 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: row;
-
+  height: 100%;
 `;
 
 export const MainWrapper = styled.div`
   display: flex;
-  background-color: #F8FBFF;
+  flex-direction: column;
+  background-color: #f8fbff;
   width: 100%;
-  height: 100vh;
+  height: 100%;
+  div:nth-child(1) {
+    font-family: "고도 B";
+    margin-top: 104px;
+    margin-left: 96.5px;
+    width: 85%
+  }
+`;
+
+export const Wrapper = styled.div`
+  margin-left: 96.5px;
+  margin-top: 91.5px;
+  width: 85%;
+  height: 100%;
 `;
