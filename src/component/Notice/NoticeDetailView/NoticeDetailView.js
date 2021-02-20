a8c3f572f88922628f1cd6e73a350932cf0b0ffc
Giljun Bae <bgj0127@gmail.com>
2021-02-20 18:25:41 +0900

    [FEAT]<component/~/NoticeDetailView> Create File

--- src/component/Notice/NoticeDetailView/NoticeDetailView.js
+++ src/component/Notice/NoticeDetailView/NoticeDetailView.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import * as S from './style';
 import Header from '../../Header/Header';
+import Vote from './Vote';
+import Common from './Common';
 
 const NoticeDetailView = () => {
   return (
---
+++ src/component/Notice/NoticeDetailView/Vote.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './style';
+
+const Vote = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Vote;
