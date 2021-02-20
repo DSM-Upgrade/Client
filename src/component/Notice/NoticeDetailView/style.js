0b3743909f6454be76afe9efc602f8af45f176cf
Giljun Bae <bgj0127@gmail.com>
2021-02-20 18:14:32 +0900

    [FEAT]<component/~/NoticeDetailView.js> Apply NoticeDetailView

--- src/component/Notice/NoticeDetailView/style.js
+++ src/component/Notice/NoticeDetailView/style.js
@@ -4,20 +4,26 @@ export const Container = styled.div`
   display: flex;
   width:100%;
   height:100%;
+  background: #F8FBFF;
 `
 
 export const Wrapper = styled.div`
-  width: 73%;
-  height: 54rem;
+  width: 72%;
+  height: 48rem;
   margin: auto;
-  background: #ccc;
+`
+
+export const Inner = styled.div`
+  width: 100%;
+  height: 45rem;
+  border: 1px solid #BEC9FF;
 `
 
 export const TitleBox = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  width: 96.45%;
+  width: 96.4%;
   height: 9.625rem;
   background: #E5EEFF;
   font-size: 32px;
@@ -28,4 +34,15 @@ export const Date = styled.div`
   font-size: 16px;
   color: #BCBCBC;
   margin-top: 10px;
+`
+
+export const Detail = styled.div`
+  width: 92.76%;
+  height: 29.126rem;
+  padding: 50px;
+  background: #fff;
+`
+
+export const File = styled.div`
+  margin-top: 20px;
 `
