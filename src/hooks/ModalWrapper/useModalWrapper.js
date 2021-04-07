8d8318a54387f8034006cca1de7da590cd5e3073
cutyapple <dbtldhs0915@naver.com>
2021-04-07 23:20:19 +0900

    [FEAT] <src/hooks/ModalWrapper/useModalWrapper.js> add for Modal Wrapper

--- src/hooks/ModalWrapper/useModalWrapper.js
+++ src/hooks/ModalWrapper/useModalWrapper.js
@@ -1,16 +1,15 @@
-import React, { useCallback } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { MODAL_ACTION_CREATERS } from "../../module/action/modal";
-import ModalWrapper from "../../component/ModalWrapper/ModalWrapper";
+import { useCallback } from "react";
 
-const ModalWrapperContainer = () => {
+import { useDispatch, useSelector } from "react-redux";
+
+import { MODAL_ACTION_CREATERS } from "../../module/action/modal";
+
+export const useModalWrapper = () => {
   const { dropModal } = MODAL_ACTION_CREATERS;
   const modalData = useSelector((state) => state.modal);
 
   const dispatch = useDispatch();
   const ModalOff = useCallback(() => dispatch(dropModal()), [dispatch]);
 
-  return <ModalWrapper modalData={modalData} ModalOff={ModalOff} />;
+  return { modalData, ModalOff };
 };
-
-export default ModalWrapperContainer;
