a5b7ff4be19ff0a9933ee705810f0d847fcbb981
cutyapple <dbtldhs0915@naver.com>
2021-04-07 17:08:42 +0900

    [REFACTOR] <src/container/~/PasswordModalContainer.js> use Hooks

--- src/container/Modal/PasswordModalContainer/PasswordModalContainer.js
+++ src/container/Modal/PasswordModalContainer/PasswordModalContainer.js
@@ -1,60 +1,13 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PasswordModal from "../../../component/Modal/PasswordModal/PasswordModal";
+import { usePasswordModal } from "../../../hooks/Modal/usePasswordModal";
 import { MODAL_ACTION_CREATERS } from "../../../module/action/modal";
 
 const PasswordModalContainer = () => {
   const errorData = useSelector((state) => state.modal.error);
 
-  const [PWInfo, setPWInfo] = useState({
-    curPW: "",
-    newPW: "",
-    rePW: "",
-    confirmInfo: { state: false, text: "" },
-  });
-  const ChangePWInfo = useCallback(
-    (e) => {
-      const { name, value } = e.target;
-
-      console.log(PWInfo);
-
-      setPWInfo((prev) => ({
-        ...prev,
-        [name]: value,
-      }));
-    },
-    [PWInfo]
-  );
-
-  const ConfirmPWInfo = useCallback(() => {
-    const { newPW, rePW } = PWInfo;
-    let confirmInfo = {
-      state: false,
-      text: "",
-    };
-
-    if (newPW === "") {
-      confirmInfo = {
-        state: true,
-        text: `"새 비밀번호"란을 입력해주세요.`,
-      };
-    } else if (rePW === "") {
-      confirmInfo = {
-        state: true,
-        text: `"새 비밀번호 확인"란을 입력해주세요.`,
-      };
-    } else if (newPW !== rePW) {
-      confirmInfo = {
-        state: true,
-        text: "비밀번호를 확인해주세요.",
-      };
-    }
-
-    setPWInfo((prev) => ({
-      ...prev,
-      confirmInfo: confirmInfo,
-    }));
-  }, [PWInfo]);
+  const { PWInfo, ChangePWInfo, ConfirmPWInfo } = usePasswordModal();
 
   const { dropModal } = MODAL_ACTION_CREATERS;
   const dispatch = useDispatch();
