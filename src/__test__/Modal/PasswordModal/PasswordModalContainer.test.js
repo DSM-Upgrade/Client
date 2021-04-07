6884f7537032fdcae11f0e108ffe37e06420e35f
cutyapple <dbtldhs0915@naver.com>
2021-04-07 18:36:18 +0900

    [TEST] <src/__test__/~/PasswordModalContainer.test.js>

---
+++ src/__test__/Modal/PasswordModal/PasswordModalContainer.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { useSelector } from "react-redux";
+
+import { render } from "@testing-library/react";
+
+import PasswordModalContainer from "../../../container/Modal/PasswordModalContainer/PasswordModalContainer";
+
+jest.mock("react-redux");
+
+describe("PasswordModalContainer", () => {
+  useSelector.mockImplementation((selector) =>
+    selector({ modal: { error: false } })
+  );
+
+  const setUp = () => {
+    const utils = render(<PasswordModalContainer />);
+
+    return { utils };
+  };
+
+  it(`match snapshot`, () => {
+    const {
+      utils: { container },
+    } = setUp();
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('click "PreventModalOff" once', () => {
+    const {
+      utils: { getByText },
+    } = setUp();
+
+    expect(getByText("비밀번호 변경"));
+  });
+});
