5b42a6280a6c878111d912bdd79ecb9b2847def2
cutyapple <dbtldhs0915@naver.com>
2021-04-08 20:12:05 +0900

    [TEST] <src/__test__/~/PasswordModalContainer.test.js> coverage 100%

--- src/__test__/container/Modal/PasswordModalContainer/PasswordModalContainer.test.js
+++ src/__test__/container/Modal/PasswordModalContainer/PasswordModalContainer.test.js
@@ -1,14 +1,17 @@
 import React from "react";
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-import { render } from "@testing-library/react";
+import { render, fireEvent, createEvent } from "@testing-library/react";
 
 import PasswordModalContainer from "../../../../container/Modal/PasswordModalContainer/PasswordModalContainer";
 
 jest.mock("react-redux");
 
 describe("PasswordModalContainer", () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
   useSelector.mockImplementation((selector) =>
     selector({ modal: { error: false } })
   );
@@ -27,11 +30,40 @@ describe("PasswordModalContainer", () => {
     expect(container).toMatchSnapshot();
   });
 
-  it('click "PreventModalOff" once', () => {
+  it("render", () => {
     const {
       utils: { getByText },
     } = setUp();
 
     expect(getByText("비밀번호 변경"));
+  });
+
+  it('click "PreventModalOff" once', () => {
+    const {
+      utils: { getByText },
+    } = setUp();
+
+    const modalOffElement = getByText("취소");
+
+    expect(dispatch).toHaveBeenCalledTimes(0);
+
+    fireEvent.click(modalOffElement);
+
+    expect(dispatch).toHaveBeenCalledTimes(1);
+  });
+
+  it("click preventModalOff", () => {
+    const {
+      utils: { getByTestId },
+    } = setUp();
+
+    const passwordModalElement = getByTestId("password-modal-element");
+    const mockClickEvent = createEvent.click(passwordModalElement);
+
+    expect(mockClickEvent.defaultPrevented).toBeFalsy();
+
+    fireEvent(passwordModalElement, myEvent);
+
+    expect(mockClickEvent.defaultPrevented).toBeTruthy();
   });
 });
