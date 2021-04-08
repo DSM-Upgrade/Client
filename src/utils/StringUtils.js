950ea3ba91257edeac33828180345b33e528aa25
cutyapple <dbtldhs0915@naver.com>
2021-04-08 10:17:27 +0900

    [FEAT] <src/utils/StringUtils.js> add removeNonNumeric

---
+++ src/utils/StringUtils.js
@@ -0,0 +1,24 @@
+export const limitMaxValue = (recivedNumber, maxValue) => {
+  if (recivedNumber < maxValue) {
+    return recivedNumber;
+  } else {
+    return maxValue;
+  }
+};
+
+export const removeNonNumeric = (targetValue) => {
+  let numericArr = [];
+  String(targetValue)
+    .split("")
+    .forEach((value, forEachIdx) => {
+      if (value === "." && !~numericArr.findIndex((num) => num === ".")) {
+        numericArr.push(value);
+      } else if (!isNaN(value)) {
+        numericArr.push(value);
+      } else if (forEachIdx === 0 && value === "-") {
+        numericArr.push(value);
+      }
+    });
+
+  return Number(numericArr.join(""));
+};
