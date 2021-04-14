c4285c963e7aec8cad4709d57add8135b678719b
JaeHyun <soehd0889@naver.com>
2021-04-14 16:45:55 +0900

    [FEAT] <src/container/HomeworkContainer/~> add container file

---
+++ src/container/HomeworkContainer/HomeworkAdminContainer/HomeworkAdminContainer.js
@@ -0,0 +1,6 @@
+import React from "react";
+import HomeworkAdmin from "../../../component/Homework/HomeworkAdmin/HomeworkAdmin";
+
+const HomeworkAdminContainer = () => {
+  return <HomeworkAdminContainer />;
+};
---
+++ src/container/HomeworkContainer/HomeworkAdminDetailViewContainer/HomeworkAdminDetailViewContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import HomeworkAdminDetailView from "../../../component/Homework/HomeworkAdminDetailView/HomeworkAdminDetailView";
+
+const HomeworkAdminDetailViewContainer = () => {
+  return <HomeworkAdminDetailView />;
+};
+
+export default HomeworkAdminDetailViewContainer;
