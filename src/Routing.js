1c7824068a0c1fc44fadfaaa7d3949c237706f12
JaeHyun <soehd0889@naver.com>
2021-04-14 16:47:03 +0900

    [FEAT] <src/Routing.js> modify routing.js

--- src/Routing.js
+++ src/Routing.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import ModalWrapperContainer from "./container/ModalWrapperContainer/ModalWrapperContainer";
-import MyPageContanier from "./container/MyPageContanier/MyPageContanier";
 import FineContainer from "./container/FineContainer/FineContainer";
 import Homework from "./container/HomeworkContainer/HomeworkContainer";
 import HomeworkDetailView from "./container/HomeworkContainer/HomeworkDetailView/HomeworkDetailViewContainer";
-import HomeworkAdmin from "./component/Homework/HomeworkAdmin/HomeworkAdmin";
-import HomeworkAdminDetailView from "./component/Homework/HomeworkAdminDetailView/HomeworkAdminDetailView";
+import HomeworkAdmin from "./container/HomeworkContainer/HomeworkAdminContainer/HomeworkAdminContainer";
+import HomeworkAdminDetailView from "./container/HomeworkContainer/HomeworkAdminDetailViewContainer/HomeworkAdminDetailViewContainer";
 
 const Routing = () => {
   return (
     <>
       <Switch>
-        <Route exact path="/mypage" component={MyPageContanier} />
         <Route exact path="/fine" component={FineContainer} />
         <Route exact path="/homework" component={Homework} />
         <Route exact path="/homeworkDetailView" component={HomeworkDetailView} />
