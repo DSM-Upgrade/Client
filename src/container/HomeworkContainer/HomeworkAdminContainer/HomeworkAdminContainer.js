567be55b95c60471f039a5cc4dd29524578ef745
JaeHyun <soehd0889@naver.com>
2021-04-14 20:13:12 +0900

    [FEAT] <src/container/HomeworkContainer/~> modify

--- src/container/HomeworkContainer/HomeworkAdminContainer/HomeworkAdminContainer.js
+++ src/container/HomeworkContainer/HomeworkAdminContainer/HomeworkAdminContainer.js
@@ -2,5 +2,7 @@ import React from "react";
 import HomeworkAdmin from "../../../component/Homework/HomeworkAdmin/HomeworkAdmin";
 
 const HomeworkAdminContainer = () => {
-  return <HomeworkAdminContainer />;
+  return <HomeworkAdmin />;
 };
+
+export default HomeworkAdminContainer;
