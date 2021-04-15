8b4be2351c0effef675ff8984a7fd7c08317ac86
JaeHyun <soehd0889@naver.com>
2021-04-15 20:25:29 +0900

    [FEAT] <src/module/~/homework> add homework module

---
+++ src/module/action/homework/index.js
@@ -0,0 +1,28 @@
+const SET_HOMEWORK_LIST = "homwork/SET_HOMEWORK_LIST";
+const setHomeworkList = (payload) => ({
+  type: SET_HOMEWORK_LIST,
+  payload,
+});
+const GET_HOMEWORK_LIST_SAGA = "homework/GET_HOMEWORK_LIST_SAGA";
+const getHomeworkListSaga = (payload) => ({
+  type: GET_HOMEWORK_LIST_SAGA,
+  payload,
+});
+
+const CHANGE_HOMEWORK_LIST_SAGA = "homework/CHANGE_HOMEWORK_LIST_SAGA";
+const changeHomeworkListSaga = (payload) => ({
+  type: CHANGE_HOMEWORK_LIST_SAGA,
+  payload,
+});
+
+export const homeworkActions = {
+  SET_HOMEWORK_LIST,
+  GET_HOMEWORK_LIST_SAGA,
+  CHANGE_HOMEWORK_LIST_SAGA,
+};
+
+export const homeworkActionCreaters = {
+  setHomeworkList,
+  getHomeworkListSaga,
+  changeHomeworkListSaga,
+};
---
+++ src/module/reducer/homework/index.js
@@ -0,0 +1,48 @@
+import { homeworkActions } from "../../action/homework";
+
+const initialState = {
+  homeworkList: {
+    id: "",
+    Title: "",
+    Start: "",
+    End: "",
+    Content: "",
+    Status: "",
+  },
+};
+
+const homeworkReducer = (state = initialState, action) => {
+  const {
+    SET_HOMEWORK_LIST,
+    GET_HOMEWORK_LIST_SAGA,
+    CHANGE_HOMEWORK_LIST_SAGA,
+  } = homeworkActions;
+
+  switch (action.type) {
+    case SET_HOMEWORK_LIST: {
+      return {
+        ...state,
+        homeworkList: action.payload,
+      };
+    }
+    /*     case GET_HOMEWORK_LIST_SAGA: {
+      return {
+        ...state,
+        homeworkList: action.payload,
+      };
+    }
+    case CHANGE_HOMEWORK_LIST_SAGA: {
+      return {
+        ...state,
+        homeworkList: action.payload,
+      };
+    } */
+    default: {
+      return {
+        ...state,
+      };
+    }
+  }
+};
+
+export default homeworkReducer;
