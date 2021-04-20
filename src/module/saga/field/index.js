b667697fbb5aea4d464e89649de8dc7c2a09aa19
cutyapple <dbtldhs0915@naver.com>
2021-04-20 20:19:47 +0900

    [MODIFY] <src/module/saga/field/index.js> bug fix

--- src/module/saga/field/index.js
+++ src/module/saga/field/index.js
@@ -3,31 +3,31 @@ import { call, takeLatest } from "redux-saga/effects";
 import { myPageApi } from "../../../lib/Api";
 import {
   methodType,
-  requestApiWithBodyWithoutToken,
+  requestApiWithoutBodyWithoutToken,
 } from "../../../lib/requestApi";
-import { FIELD_ACTION_CREATERS } from "../../action/field";
+import { FIELD_ACTIONS } from "../../action/field";
 
 function* fetchFields() {
   try {
     const REQUEST_URL = myPageApi.field();
     const HTTP_METHOD = methodType.GET;
 
-    const res = yield call(
-      requestApiWithBodyWithoutToken,
-      REQUEST_URL,
-      HTTP_METHOD
-    );
+    // const res = yield call(
+    //   requestApiWithoutBodyWithoutToken,
+    //   HTTP_METHOD,
+    //   REQUEST_URL
+    // );
 
-    console.log(res);
+    // console.log(res);
   } catch (error) {
     console.error(error);
   }
 }
 
 function* fieldSaga() {
-  const { fetchFieldsSaga } = FIELD_ACTION_CREATERS;
+  const { FETCH_FIELDS_SAGA } = FIELD_ACTIONS;
 
-  yield takeLatest(fetchFieldsSaga, fetchFields);
+  yield takeLatest(FETCH_FIELDS_SAGA, fetchFields);
 }
 
 export default fieldSaga;
