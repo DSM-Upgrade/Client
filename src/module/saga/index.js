99f490f68e4dca0039213eabb1e594b4f457518e
cutyapple <dbtldhs0915@naver.com>
2021-04-20 21:38:59 +0900

    [FEAT] <src/module/saga/index.js> apply myPageSaga

--- src/module/saga/index.js
+++ src/module/saga/index.js
@@ -1,9 +1,10 @@
 import { all } from "redux-saga/effects";
 
 import fieldSaga from "./field";
+import myPageSaga from "./myPage";
 
 function* rootSaga() {
-  yield all([fieldSaga()]);
+  yield all([fieldSaga(), myPageSaga()]);
 }
 
 export default rootSaga;
