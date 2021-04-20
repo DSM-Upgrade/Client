c71c5f6150c7105e9afe516052b9ce00dade23f8
Giljun Bae <bgj0127@gmail.com>
2021-04-20 18:55:29 +0900

    [FIX]<src/component/~/Main> Change how images are processed

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import * as S from './style';
-import { mainBackground } from '../../assets/main';
+// import { mainBackground } from '../../assets/main';
 import Header from "../Header/Header";
 import Footer from "./Footer/Footer";
 import Slider from './Slide/Slider';
 
-const Main = () => {
+const Main = ({img}) => {
   return (
     <S.Main>
       <Header />
       <S.Container>
           <S.Inner>
-            <S.BackgroundImg img={mainBackground}></S.BackgroundImg>
+            <S.BackgroundImg img={img}></S.BackgroundImg>
             <S.SlideWrapper>
               <Slider />
             </S.SlideWrapper>
