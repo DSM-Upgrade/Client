9a2f9c0b7a25576fbdd28e2fc75843bccca2e9a3
cutyapple <dbtldhs0915@naver.com>
2021-04-20 21:39:42 +0900

    [FEAT] <src/module/reducer/myPage/index.js> apply GET_STD_SAGA_SUCCESS

--- src/module/reducer/myPage/index.js
+++ src/module/reducer/myPage/index.js
@@ -10,10 +10,11 @@ const initialState = {
 };
 
 const myPageReducer = (state = initialState, action) => {
-  const { SET_STD_INFO } = myPageActions;
+  const { SET_STD_INFO, GET_STD_INFO_SAGA_SUCCESS } = myPageActions;
 
   switch (action.type) {
-    case SET_STD_INFO: {
+    case SET_STD_INFO:
+    case GET_STD_INFO_SAGA_SUCCESS: {
       return {
         ...state,
         stdInfo: action.payload,
