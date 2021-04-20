956c31c0a8c6f2054034ac1268df86960ed2fdbe
Giljun Bae <bgj0127@gmail.com>
2021-04-20 18:56:10 +0900

    [FEAT]<src/Routing> Add Notice Router

--- src/Routing.js
+++ src/Routing.js
@@ -2,7 +2,7 @@ import React from "react";
 import MainContainer from "./container/MainContainer/MainContainer";
 
 import { Route, Switch } from "react-router-dom";
-
+import NoticeContainer from "./container/NoticeContainer/NoticeContainer";
 import MyPageContanier from "./container/MyPageContanier/MyPageContanier";
 import FineContainer from "./container/FineContainer/FineContainer";
 import Homework from "./component/Homework/Homework";
@@ -16,6 +16,7 @@ const Routing = () => {
     <>
       <Switch>
         <Route exact path="/" component={MainContainer}/>
+        <Route exact path="/notice" component={NoticeContainer}/>
         <Route exact path="/mypage" component={MyPageContanier} />
         <Route exact path="/fine" component={FineContainer} />
         <Route exact path="/homework" component={Homework} />
