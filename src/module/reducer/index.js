93f0b2acf785159988a63f1c004f420d96be3647
cutyapple <dbtldhs0915@naver.com>
2021-04-20 19:53:08 +0900

    [FEAT] <src/module/reducer/index.js> apply field and loginUser reducer

--- src/module/reducer/index.js
+++ src/module/reducer/index.js
@@ -1,7 +1,12 @@
 import { combineReducers } from "redux";
+
+import field from "./field";
 import modal from "./modal";
+import loginUser from "./loginUser";
 
 const rootReducer = combineReducers({
+  field,
+  loginUser,
   modal,
 });
 
