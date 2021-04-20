96466adf70b02595ea73e98c6e44019bc54c360a
cutyapple <dbtldhs0915@naver.com>
2021-04-20 21:37:17 +0900

    [FEAT] <src/page/~/MyPagePage.js> dispatch getStdInfoSaga

--- src/page/MyPagePage/MyPagePage.js
+++ src/page/MyPagePage/MyPagePage.js
@@ -2,7 +2,8 @@ import React, { useCallback, useEffect } from "react";
 
 import { useDispatch } from "react-redux";
 
-import { FIELD_ACTION_CREATERS } from "../../module/action/field";
+import { fieldActionCreaters } from "../../module/action/field";
+import { myPageActionCreaters } from "../../module/action/myPage";
 
 import * as S from "./style";
 
@@ -12,14 +13,19 @@ import MyPageContanier from "../../container/MyPageContanier/MyPageContanier";
 const MyPagePage = () => {
   const dispatch = useDispatch();
 
-  const { fetchFieldsSaga } = FIELD_ACTION_CREATERS;
+  const { fetchFieldsSaga } = fieldActionCreaters;
+  const { getStdInfoSaga } = myPageActionCreaters;
 
   const dispatchFetchFieldSaga = useCallback(() => {
     dispatch(fetchFieldsSaga());
   }, [dispatch]);
+  const dispatchGetStdInfoSaga = useCallback(() => {
+    dispatch(getStdInfoSaga());
+  }, [dispatch]);
 
   useEffect(() => {
     dispatchFetchFieldSaga();
+    dispatchGetStdInfoSaga();
   }, []);
 
   return (
