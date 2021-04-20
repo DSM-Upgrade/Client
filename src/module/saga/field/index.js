f3da7156b87cfb3fcefe908e143509fe59612ee6
cutyapple <dbtldhs0915@naver.com>
2021-04-20 21:39:21 +0900

    [FEAT] <src/module/saga/field/index.js> apply FETCH_FIELD_SAGA_SUCCESS

--- src/module/saga/field/index.js
+++ src/module/saga/field/index.js
@@ -1,31 +1,37 @@
-import { call, takeLatest } from "redux-saga/effects";
+import { call, put, takeLatest } from "redux-saga/effects";
 
 import { myPageApi } from "../../../lib/Api";
 import {
   methodType,
   requestApiWithoutBodyWithoutToken,
 } from "../../../lib/requestApi";
-import { FIELD_ACTIONS } from "../../action/field";
+
+import { fieldActions } from "../../action/field";
 
 function* fetchFields() {
   try {
-    const REQUEST_URL = myPageApi.field();
+    const { FETCH_FIELDS_SAGA_SUCCESS } = fieldActions;
+
     const HTTP_METHOD = methodType.GET;
+    const REQUEST_URL = myPageApi.field();
 
-    // const res = yield call(
-    //   requestApiWithoutBodyWithoutToken,
-    //   HTTP_METHOD,
-    //   REQUEST_URL
-    // );
+    const res = yield call(
+      requestApiWithoutBodyWithoutToken,
+      HTTP_METHOD,
+      REQUEST_URL
+    );
 
-    // console.log(res);
+    yield put({
+      type: FETCH_FIELDS_SAGA_SUCCESS,
+      payload: res.data,
+    });
   } catch (error) {
     console.error(error);
   }
 }
 
 function* fieldSaga() {
-  const { FETCH_FIELDS_SAGA } = FIELD_ACTIONS;
+  const { FETCH_FIELDS_SAGA } = fieldActions;
 
   yield takeLatest(FETCH_FIELDS_SAGA, fetchFields);
 }
