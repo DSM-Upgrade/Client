76918b335b8a10f49789822c29e614cad4bd1454
CutyApple <dbtldhs0915@naver.com>
2021-04-23 07:59:04 +0900

    [FEAT] <src/module/reducer/index.js> apply auth reducer to root reducer

--- src/module/reducer/index.js
+++ src/module/reducer/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 
+import auth from "./auth";
 import field from "./field";
 import loginUser from "./loginUser";
 import modal from "./modal";
 
 const rootReducer = combineReducers({
+  auth,
   field,
   loginUser,
   modal,
