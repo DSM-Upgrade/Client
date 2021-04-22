3a7145a93a5d4071f18a39e7070a08bac0b782db
CutyApple <dbtldhs0915@naver.com>
2021-04-23 08:45:42 +0900

    [FEAT] <src/module/saga/loginUser/index.js> apply modify std info

--- src/module/saga/loginUser/index.js
+++ src/module/saga/loginUser/index.js
@@ -3,6 +3,7 @@ import { call, put, takeLatest } from "redux-saga/effects";
 import { myPageApi } from "../../../lib/Api";
 import {
   methodType,
+  requestApiWithBodyWithToken,
   requestApiWithoutBodyWithToken,
 } from "../../../lib/requestApi";
 
@@ -30,10 +31,51 @@ function* fetchStdInfo() {
   }
 }
 
+function* modfyStdInfo(action) {
+  try {
+    const { student_num, field_id } = action.payload;
+
+    const HTTP_METHOD = methodType.PATCH;
+    const REQUEST_URL = myPageApi.chagneStdInfo();
+    const REQUEST_BODY = { student_num, field_id };
+
+    const res = yield call(
+      requestApiWithBodyWithToken,
+      HTTP_METHOD,
+      REQUEST_URL,
+      REQUEST_BODY
+    );
+
+    alert("정보 변경에 성공했습니다.");
+  } catch (error) {
+    const { code, status } = error.data;
+
+    const code2InputFieldMap = {
+      C002: "학번",
+      F002: "분야",
+    };
+
+    const invalidInputValue = () => {
+      const targetInputName = code2InputFieldMap[code];
+      alert(`${targetInputName} 칸의 입력을 확인해주세요.`);
+    };
+
+    switch (status) {
+      case 400:
+        invalidInputValue();
+        break;
+      default:
+    }
+
+    console.log(error);
+  }
+}
+
 function* loginUserSaga() {
-  const { FETCH_STD_INFO_SAGA } = loginUserActions;
+  const { FETCH_STD_INFO_SAGA, MODIFY_STD_INFO_SAGA } = loginUserActions;
 
   yield takeLatest(FETCH_STD_INFO_SAGA, fetchStdInfo);
+  yield takeLatest(MODIFY_STD_INFO_SAGA, modfyStdInfo);
 }
 
 export default loginUserSaga;
