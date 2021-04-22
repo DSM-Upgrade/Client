f7b37803b29622ff9437f5fc937a115cd25001f5
CutyApple <dbtldhs0915@naver.com>
2021-04-23 08:12:30 +0900

    [REFACTOR] <src/~/usePasswordModal.js> refactor hooks

--- src/hooks/Modal/usePasswordModal.js
+++ src/hooks/Modal/usePasswordModal.js
@@ -1,70 +1,24 @@
-import { useState, useCallback } from "react";
-
-import { areEqual, parameterIsEmpty } from "../../utils/parameterUtils";
+import { useState } from "react";
 
 export const usePasswordModal = () => {
-  const [pwInfo, setPwInfo] = useState({
-    curPW: "",
-    newPW: "",
-    rePW: "",
-    confirmInfo: { state: false, text: "" },
+  const [passwordInfo, setPasswordInfo] = useState({
+    nowPassword: "",
+    newPassword: "",
+    reEnterPassword: "",
   });
 
-  const changePwInfo = useCallback(
-    (e) => {
-      const { name, value } = e.target;
-      setPwInfo((prev) => ({
-        ...prev,
-        [name]: value,
-      }));
-    },
-    [pwInfo]
-  );
-  const pwInfoWithUpdateConfirmInfo = useCallback(() => {
-    const confirmInfo = confirmInfoVertifiedPwInfo();
+  const changePasswordInfo = (e) => {
+    const { name, value } = e.target;
 
-    setPwInfo((prev) => ({
+    setPasswordInfo((prev) => ({
       ...prev,
-      confirmInfo,
+      [name]: value,
     }));
-  }, []);
-  const confirmInfoVertifiedPwInfo = useCallback(() => {
-    const { curPW, newPW, rePW } = pwInfo;
-    const confirmInfo = {
-      state: false,
-      text: "",
-    };
-
-    if (parameterIsEmpty(curPW)) {
-      confirmInfo = {
-        state: true,
-        text: `"현재 비밀번호"란을 입력해주세요.`,
-      };
-    } else if (parameterIsEmpty(newPW)) {
-      confirmInfo = {
-        state: true,
-        text: `"새 비밀번호"란을 입력해주세요.`,
-      };
-    } else if (parameterIsEmpty(rePW)) {
-      confirmInfo = {
-        state: true,
-        text: `"새 비밀번호 확인"란을 입력해주세요.`,
-      };
-    } else if (!areEqual(newPW, rePW)) {
-      confirmInfo = {
-        state: true,
-        text: "비밀번호를 확인해주세요.",
-      };
-    }
-
-    return confirmInfo;
-  }, [pwInfo]);
+  };
 
   return {
-    pwInfo,
-    setPwInfo,
-    changePwInfo,
-    confirmInfoVertifiedPwInfo,
-    pwInfoWithUpdateConfirmInfo,
+    passwordInfo,
+    setPasswordInfo,
+    changePasswordInfo,
   };
 };
