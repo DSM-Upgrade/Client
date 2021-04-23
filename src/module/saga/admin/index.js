02fb31d94eef0c2a986e8f4a68c9e0dcd833dd25
CutyApple <dbtldhs0915@naver.com>
2021-04-23 19:35:54 +0900

    [FEAT] <src/module/saga/admin/index.js> add fetch auth access

--- src/module/saga/admin/index.js
+++ src/module/saga/admin/index.js
@@ -21,6 +21,8 @@ function* fetchAuthList() {
 
     const { FETCH_AUTH_LIST_SAGA_SUCCESS } = adminActions;
 
+    console.log(res);
+
     yield put({
       type: FETCH_AUTH_LIST_SAGA_SUCCESS,
       payload: res.data,
@@ -30,10 +32,34 @@ function* fetchAuthList() {
   }
 }
 
+function* fetchAuthAccess(action) {
+  try {
+    const { id } = action.payload;
+
+    const HTTP_METHOD = methodType.POST;
+    const REQUEST_URL = adminApi.accessAuth();
+    const REQUEST_BODY = {
+      id,
+    };
+
+    const res = yield call(
+      requestApiWithoutBodyWithToken,
+      HTTP_METHOD,
+      REQUEST_URL,
+      REQUEST_BODY
+    );
+
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 function* adminSaga() {
   const { FETCH_AUTH_LIST_SAGA, FETCH_AUTH_ACCESS_SAGA } = adminActions;
 
   yield takeLatest(FETCH_AUTH_LIST_SAGA, fetchAuthList);
+  yield takeLatest(FETCH_AUTH_ACCESS_SAGA, fetchAuthAccess);
 }
 
 export default adminSaga;
