359efa510eac2eba81ef7e06cf2408d5084a4d3f
CutyApple <dbtldhs0915@naver.com>
2021-04-23 19:36:33 +0900

    [FEAT] <src/module/saga/index.js> apply admin Saga

--- src/module/saga/index.js
+++ src/module/saga/index.js
@@ -1,11 +1,12 @@
 import { all } from "redux-saga/effects";
 
+import adminSaga from "./admin";
 import authSaga from "./auth";
 import fieldSaga from "./field";
 import loginUserSaga from "./loginUser";
 
 function* rootSaga() {
-  yield all([authSaga(), fieldSaga(), loginUserSaga()]);
+  yield all([adminSaga(), authSaga(), fieldSaga(), loginUserSaga()]);
 }
 
 export default rootSaga;
