32b862688899cd2ea81736e3d951cbcff551897b
CutyApple <dbtldhs0915@naver.com>
2021-04-23 10:07:23 +0900

    [FEAT] <src/module/reducer/index.js> apply admin reducer to root reducer

--- src/module/reducer/index.js
+++ src/module/reducer/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 
+import admin from "./admin";
 import auth from "./auth";
 import field from "./field";
 import loginUser from "./loginUser";
 import modal from "./modal";
 
 const rootReducer = combineReducers({
+  admin,
   auth,
   field,
   loginUser,
