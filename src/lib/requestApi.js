63befa2a108c03e91f1478af6aec29c5c8ec8bfa
CutyApple <dbtldhs0915@naver.com>
2021-04-23 10:06:05 +0900

    [FEAT] <src/lib/requestApi.js> apply admin token

--- src/lib/requestApi.js
+++ src/lib/requestApi.js
@@ -56,8 +56,10 @@ export const requestApiWithoutBodyWithoutToken = async (
 export const requestApiWithoutBodyWithToken = async (method, url, header) => {
   try {
     // const accessToken = getItem(ACCESS_TOKEN)
-    const accessToken =
-      "eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MTg5MjYwOTcsImV4cCI6MTYxODkyNzg5NzAwMCwic3ViIjoiZGtzc3VkOTU1NiIsInR5cGUiOiJhY2Nlc3NfdG9rZW4ifQ.lEWjXbw9flDOhgTb6f0VKBUoVhrO5PSPtzVjs_j9hws";
+    // const accessToken = // user token
+    //   "eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MTg5MjYwOTcsImV4cCI6MTYxODkyNzg5NzAwMCwic3ViIjoiZGtzc3VkOTU1NiIsInR5cGUiOiJhY2Nlc3NfdG9rZW4ifQ.lEWjXbw9flDOhgTb6f0VKBUoVhrO5PSPtzVjs_j9hws";
+    const accessToken = // admin token
+      "eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MTg5MjYwOTcsImV4cCI6MTYxODkyNzg5NzAwMCwic3ViIjoiYWRtaW5qamFuZyIsInR5cGUiOiJhY2Nlc3NfdG9rZW4ifQ.WfnwQSPiApP03Ypebez6hyK6SPhawNhfsirGHTtSjUk";
 
     const res = await axios[method](BASE_URL + url, {
       headers: {
@@ -79,9 +81,10 @@ export const requestApiWithBodyWithToken = async (
   header
 ) => {
   try {
-    // const accessToken = getItem(ACCESS_TOKEN);
-    const accessToken =
-      "eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MTg5MjYwOTcsImV4cCI6MTYxODkyNzg5NzAwMCwic3ViIjoiZGtzc3VkOTU1NiIsInR5cGUiOiJhY2Nlc3NfdG9rZW4ifQ.lEWjXbw9flDOhgTb6f0VKBUoVhrO5PSPtzVjs_j9hws";
+    // const accessToken = // user token
+    //   "eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MTg5MjYwOTcsImV4cCI6MTYxODkyNzg5NzAwMCwic3ViIjoiZGtzc3VkOTU1NiIsInR5cGUiOiJhY2Nlc3NfdG9rZW4ifQ.lEWjXbw9flDOhgTb6f0VKBUoVhrO5PSPtzVjs_j9hws";
+    const accessToken = // admin token
+      "eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MTg5MjYwOTcsImV4cCI6MTYxODkyNzg5NzAwMCwic3ViIjoiYWRtaW5qamFuZyIsInR5cGUiOiJhY2Nlc3NfdG9rZW4ifQ.WfnwQSPiApP03Ypebez6hyK6SPhawNhfsirGHTtSjUk";
 
     const res = await axios[method](BASE_URL + url, body, {
       headers: {
