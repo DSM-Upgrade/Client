7c6e7e2685363684b9e4085de890ae96b7377755
CutyApple <dbtldhs0915@naver.com>
2021-04-23 10:09:44 +0900

    [FEAT] <src/module/saga/admin/index.js> apply fetch auth list success

---
+++ src/module/saga/admin/index.js
@@ -0,0 +1,39 @@
+import { call, put, takeLatest } from "redux-saga/effects";
+
+import { adminApi } from "../../../lib/Api";
+import {
+  methodType,
+  requestApiWithoutBodyWithToken,
+} from "../../../lib/requestApi";
+
+import { adminActions } from "../../action/admin";
+
+function* fetchAuthList() {
+  try {
+    const HTTP_METHOD = methodType.GET;
+    const REQUEST_URL = adminApi.getAuthList();
+
+    const res = yield call(
+      requestApiWithoutBodyWithToken,
+      HTTP_METHOD,
+      REQUEST_URL
+    );
+
+    const { FETCH_AUTH_LIST_SAGA_SUCCESS } = adminActions;
+
+    yield put({
+      type: FETCH_AUTH_LIST_SAGA_SUCCESS,
+      payload: res.data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function* adminSaga() {
+  const { FETCH_AUTH_LIST_SAGA, FETCH_AUTH_ACCESS_SAGA } = adminActions;
+
+  yield takeLatest(FETCH_AUTH_LIST_SAGA, fetchAuthList);
+}
+
+export default adminSaga;
