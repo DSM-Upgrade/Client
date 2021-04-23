95abb78a97b9bcd8042be0802513697861c44ed5
CutyApple <dbtldhs0915@naver.com>
2021-04-23 10:05:31 +0900

    [REFACTOR] <src/App.js> refactor import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+
 import { BrowserRouter } from "react-router-dom";
+
 import { GlobalStyle } from "./GlobalStyle";
+
 import Routing from "./Routing";
-import RegisterPage from './component/LoginPage/LoginPage'
 import LoginPage from "./component/LoginPage/LoginPage";
 
 const App = () => {
