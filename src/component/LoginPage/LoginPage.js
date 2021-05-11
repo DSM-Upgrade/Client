00cdb751c0ef8b4d9b1a72390d7daf3e9a6fda08
JaeHyun <soehd0889@naver.com>
2021-05-11 19:19:29 +0900

    [FEAT] <src/component/LoginPage> set form logininfo

--- src/component/LoginPage/LoginPage.js
+++ src/component/LoginPage/LoginPage.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderContainer from "../../container/HeaderContainer/HeaderContainer";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import * as S from "./style";
 
-
 const LoginPage = () => {
+  const [loginValues, setLoginValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("submit");
+    console.log(loginValues);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginValues({ ...loginValues, [name]: value });
+  };
+
   return (
     <S.MainWrapper>
       <HeaderContainer />
       <S.MainBox>
-        <S.LoginBox>
+        <S.LoginBox onSubmit={handleSubmit}>
           <h1>LOGIN</h1>
-          <input placeholder="id" />
-          <input type="password" placeholder="password" />
-          <button>로그인</button>
-          <Link to='/register'>회원가입</Link>
+          <input
+            placeholder="id"
+            value={loginValues.username}
+            name="username"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={loginValues.password}
+            placeholder="password"
+            onChange={handleChange}
+          />
+          <input type="submit" className="submitButton" value="로그인" />
+          <Link to="/register">회원가입</Link>
         </S.LoginBox>
         <div></div>
         <div></div>
