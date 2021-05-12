2dfc1295660175072fff8ba738fdb1c2d5d9e5ee
JaeHyun <soehd0889@naver.com>
2021-05-12 23:32:21 +0900

    [FEAT] <src/module/~/action/loginPage> action type and set initialState

--- src/module/action/loginPage/index.js
+++ src/module/action/loginPage/index.js
@@ -1,3 +1,15 @@
+const AUTH_LOG_IN = "loginPage/AUTH_LOG_IN";
+const authLogIn = (payload) => ({
+  type: AUTH_LOG_IN,
+  payload,
+});
+
+const AUTH_SIGN_UP = "loginPage/AUTH_SIGN_UP";
+const authSignUp = (payload) => ({
+  type: AUTH_SIGN_UP,
+  payload,
+});
+
 const SET_AUTH_TOKEN = "loginPage/SET_AUTH_TOKEN";
 const setAuthToken = (payload) => ({
   type: SET_AUTH_TOKEN,
@@ -6,16 +18,20 @@ const setAuthToken = (payload) => ({
 
 const FETCH_AUTH_TOKEN = "loginPage/FETCH_AUTH_TOKEN";
 const fetchAuthToken = (payload) => ({
-  type: POST_REFRESH_TOKEN,
+  type: FETCH_AUTH_TOKEN,
   payload,
 });
 
 export const loginPageActions = {
   SET_AUTH_TOKEN,
   FETCH_AUTH_TOKEN,
+  AUTH_LOG_IN,
+  AUTH_SIGN_UP,
 };
 
 export const loginPageActionsCreaters = {
   setAuthToken,
   fetchAuthToken,
+  authLogIn,
+  authSignUp,
 };
