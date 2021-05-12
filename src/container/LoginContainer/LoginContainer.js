f83d64dc329b9464d0d5c317445efeb47977fc87
JaeHyun <soehd0889@naver.com>
2021-05-12 23:29:09 +0900

    [FEAT] <src/~/LoginContainer> LoginPage dispatch

--- src/container/LoginContainer/LoginContainer.js
+++ src/container/LoginContainer/LoginContainer.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+
+import { loginPageActionsCreaters } from "../../module/action/loginPage/index";
+
 import LoginPage from "../../component/LoginPage/LoginPage";
 
 const LoginContainer = () => {
+  const dispatch = useDispatch();
+  const loginInfo = useSelector((state) => state.loginPage.loginInfo);
+
+  const [loginData, setLoginData] = useState({
+    ...loginInfo,
+  });
+
+  const { authLogIn } = loginPageActionsCreaters;
+
+  const setLoginAuthInfo = (authLoginInfo) => {
+    dispatch(authLogIn(authLoginInfo));
+  };
+
+  const onChangeLoginData = (e) => {
+    const { name, value } = e.target;
+    setLoginData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const onSubmitLoginFormData = (e) => {
+    e.preventDefault();
+    /* const { username, password } = loginData;*/
+    setLoginAuthInfo(loginData);
+    console.log(loginInfo);
+  };
+
+  useEffect(() => {
+    setLoginData(loginInfo);
+  }, [loginInfo]);
+
   return (
-    <>
-      <LoginPage />
-    </>
+    <LoginPage
+      loginData={loginData}
+      onSubmitLoginFormData={onSubmitLoginFormData}
+      onChangeLoginData={onChangeLoginData}
+    />
   );
 };
-
 export default LoginContainer;
