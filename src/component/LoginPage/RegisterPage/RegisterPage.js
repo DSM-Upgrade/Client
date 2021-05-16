cf990f12d85f153e802939c8702e8afceed36af5
JaeHyun <soehd0889@naver.com>
2021-05-16 22:44:44 +0900

    [MODIFY] <src/componet/~/Register> modify style, add test code

--- src/component/LoginPage/RegisterPage/RegisterPage.js
+++ src/component/LoginPage/RegisterPage/RegisterPage.js
@@ -1,25 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderContainer from "../../../container/HeaderContainer/HeaderContainer";
+import { Link } from "react-router-dom";
 import * as S from "./style";
 
 const RegisterPage = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password: "",
+    name: "",
+    field_id: "",
+    student_num: "",
+  });
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(signUpData);
+  };
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setSignUpData({
+      ...signUpData,
+      [name]: value,
+    });
+  };
+
   return (
     <S.MainWrapper>
       <HeaderContainer />
       <S.MainBox>
-        <S.RegisterBox>
+        <S.RegisterBox onSubmit={onSubmitHandler}>
           <h1>Register</h1>
           <div className="firstLine">
-            <input placeholder="학년" maxLength="1"></input>
-            <input placeholder="반" maxLength="1"></input>
-            <input placeholder="번호" maxLength="2"></input>
-            <input placeholder="이름"></input>
+            <input
+              name="student_num"
+              value={signUpData.student_num}
+              onChange={onChangeHandler}
+              placeholder="학번"
+              maxLength="4"
+            ></input>
+            <input
+              name="name"
+              value={signUpData.name}
+              onChange={onChangeHandler}
+              placeholder="이름"
+            ></input>
           </div>
           <div className="anotherLine">
-            <input placeholder="아이디 (6 ~ 16자리, 영어, 숫자)"></input>
-            <input placeholder="비밀번호 (8 ~ 20자리, 영어, 숫자, 특수문자)"></input>
-            <select name="Field">
-              <option value="">분야</option>
+            <input
+              name="username"
+              value={signUpData.username}
+              onChange={onChangeHandler}
+              placeholder="아이디 (6 ~ 16자리, 영어, 숫자)"
+            ></input>
+            <input
+              name="password"
+              value={signUpData.password}
+              onChange={onChangeHandler}
+              placeholder="비밀번호 (8 ~ 20자리, 영어, 숫자, 특수문자)"
+            ></input>
+            <select
+              name="field_id"
+              value={signUpData.field_id}
+              onChange={onChangeHandler}
+            >
+              <option>분야</option>
               <option value="frontEnd">웹 프론트엔드</option>
               <option value="backEnd">웹 백엔드</option>
               <option value="android">안드로이드</option>
@@ -27,8 +72,8 @@ const RegisterPage = () => {
               <option value="design">디자인</option>
             </select>
           </div>
-          <button>회원가입</button>
-          <a href="#">로그인</a>
+          <input type="submit" className="submitButton" value="회원가입" />
+          <Link to="/login">로그인</Link>
         </S.RegisterBox>
         <div className="backgroundTriangle">
           <div></div>
