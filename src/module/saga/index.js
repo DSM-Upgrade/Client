9efa2cbbed20b63fd681cf89ec8d8acede70d8ec
JaeHyun <soehd0889@naver.com>
2021-05-20 20:09:22 +0900

    [FEAT] <src/~/saga/index> add loginpagesaga to rootsaga

--- src/module/saga/index.js
+++ src/module/saga/index.js
@@ -3,10 +3,17 @@ import { all } from "redux-saga/effects";
 import adminSaga from "./admin";
 import authSaga from "./auth";
 import fieldSaga from "./field";
+import loginPageSaga from "./loginPage";
 import loginUserSaga from "./loginUser";
 
 function* rootSaga() {
-  yield all([adminSaga(), authSaga(), fieldSaga(), loginUserSaga()]);
+  yield all([
+    adminSaga(),
+    authSaga(),
+    fieldSaga(),
+    loginUserSaga(),
+    loginPageSaga(),
+  ]);
 }
 
 export default rootSaga;
