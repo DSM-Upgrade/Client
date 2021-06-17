c8a37957ed99a5733c0d042a9441926ff6800fe1
JaeHyun <soehd0889@naver.com>
2021-06-17 17:25:27 +0900

    [FEAT] <src/container/homeworkContainer> add homeworkcontainer code

--- src/container/HomeworkContainer/HomeworkContainer.js
+++ src/container/HomeworkContainer/HomeworkContainer.js
@@ -1,33 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Homework from "../../component/Homework/Homework";
+import { homeworkActionsCreaters } from "../../module/action/homework";
 
-const homeworkList = [
-  {
-    Id: 1,
-    Title: "숙제 제목",
-    Start: "숙제 등록한 날짜",
-    End: "숙제 기한 날짜",
-    Content: "숙제 내용",
-    Status: "ASSIGNED", // 숙제의 상태 (ASSIGNED, SUBMITTED, UN_SUBMITTED, FINISHED)
-  },
-  {
-    Id: 2,
-    Title: "숙제 제목",
-    Start: "숙제 등록한 날짜",
-    End: "숙제 기한 날짜",
-    Content: "숙제 내용",
-    Status: "SUBMITTED", // 숙제의 상태 (ASSIGNED, SUBMITTED, UN_SUBMITTED, FINISHED)
-  },
-  {
-    Id: 3,
-    Title: "숙제 제목",
-    Start: "숙제 등록한 날짜",
-    End: "숙제 기한 날짜",
-    Content: "숙제 내용",
-    Status: "UN_SUBMITTED", // 숙제의 상태 (ASSIGNED, SUBMITTED, UN_SUBMITTED, FINISHED)
-  },
-];
 const HomeworkContainer = () => {
+  const dispatch = useDispatch();
+  const homeworkData = useSelector((state) => state.homework.homeworkList);
+
+  const [homeworkList, setHomeworkList] = useState({
+    ...homeworkData,
+  });
+
+  const { getHomeworkListSaga } = homeworkActionsCreaters;
+
+  const getHomeworkList = () => {
+    dispatch(getHomeworkListSaga());
+  };
+
+  useEffect(() => {
+    setHomeworkList(homeworkData);
+  }, [homeworkData]);
+  useEffect(() => {
+    getHomeworkList();
+  }, []);
+
   return <Homework homeworkList={homeworkList} />;
 };
 
