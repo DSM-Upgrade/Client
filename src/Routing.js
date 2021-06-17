efa2f44356793bb343b338a0eaeadbe0ee299a24
JaeHyun <soehd0889@naver.com>
2021-06-17 09:29:40 +0900

    [MODIFY] <src/Routing.js> modify code

--- src/Routing.js
+++ src/Routing.js
@@ -1,21 +1,35 @@
 import React from "react";
+import MainContainer from "./container/MainContainer/MainContainer";
+
 import { Route, Switch } from "react-router-dom";
-import ModalWrapperContainer from "./container/ModalWrapperContainer/ModalWrapperContainer";
+
 import FineContainer from "./container/FineContainer/FineContainer";
-import Homework from "./container/HomeworkContainer/HomeworkContainer";
-import HomeworkDetailView from "./container/HomeworkContainer/HomeworkDetailView/HomeworkDetailViewContainer";
-import HomeworkAdmin from "./container/HomeworkContainer/HomeworkAdminContainer/HomeworkAdminContainer";
-import HomeworkAdminDetailView from "./container/HomeworkContainer/HomeworkAdminDetailViewContainer/HomeworkAdminDetailViewContainer";
+import Homework from "./component/Homework/Homework";
+import HomeworkDetailView from "./component/Homework/HomeworkDetailView/HomeworkDetailView";
+import HomeworkAdmin from "./component/Homework/HomeworkAdmin/HomeworkAdmin";
+import HomeworkAdminDetailView from "./component/Homework/HomeworkAdminDetailView/HomeworkAdminDetailView";
+import ModalWrapperContainer from "./container/ModalWrapper/ModalWrapperContainer";
+import MyPagePage from "./page/MyPagePage/MyPagePage";
 
 const Routing = () => {
   return (
     <>
       <Switch>
+        <Route exact path="/" component={MainContainer} />
+        <Route exact path="/mypage" component={MyPagePage} />
         <Route exact path="/fine" component={FineContainer} />
         <Route exact path="/homework" component={Homework} />
-        <Route exact path="/homeworkDetailView/:id" component={HomeworkDetailView} />
+        <Route
+          exact
+          path="/homeworkDetailView"
+          component={HomeworkDetailView}
+        />
         <Route exact path="/homeworkAdmin" component={HomeworkAdmin} />
-        <Route exact path="/homeworkAdminDetailView" component={HomeworkAdminDetailView} />
+        <Route
+          exact
+          path="/homeworkAdminDetailView"
+          component={HomeworkAdminDetailView}
+        />
       </Switch>
       <ModalWrapperContainer />
     </>
