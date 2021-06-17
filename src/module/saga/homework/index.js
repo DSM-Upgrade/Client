e2640dafd306359887b6cebbe7c17fa95ce0cf8b
JaeHyun <soehd0889@naver.com>
2021-06-17 17:26:08 +0900

    [FEAT] <src/module/saga/homework> add saga code

--- src/module/saga/homework/index.js
+++ src/module/saga/homework/index.js
@@ -1,2 +1,42 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 
+import { homeworkApi, myPageApi } from "../../../lib/Api";
+import {
+  requestApiWithoutBodyWithToken,
+  requestApiWithBodyWithToken,
+  methodType,
+} from "../../../lib/requestApi";
+
+import { homeworkActions } from "../../action/homework/index";
+
+function* getHomeworkList(action) {
+  console.log(action.payload);
+  try {
+    const HTTP_METHOD = methodType.GET;
+    const REQUEST_URL = homeworkApi.getHomeworkList();
+
+    const res = yield call(
+      requestApiWithoutBodyWithToken,
+      HTTP_METHOD,
+      REQUEST_URL
+    );
+
+    const { GET_HOMEWORK_LIST } = homeworkActions;
+
+    yield put({
+      type: GET_HOMEWORK_LIST,
+      payload: res.data,
+    });
+    console.log(`리스트 받아옴`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function* homeworkSaga() {
+  const { GET_HOMEWORK_LIST_SAGA } = homeworkActions;
+
+  yield takeLatest(GET_HOMEWORK_LIST_SAGA, getHomeworkList);
+}
+
+export default homeworkSaga;
--- src/module/saga/index.js
+++ src/module/saga/index.js
@@ -4,9 +4,16 @@ import adminSaga from "./admin";
 import authSaga from "./auth";
 import fieldSaga from "./field";
 import loginUserSaga from "./loginUser";
+import homeworkSaga from "./homework";
 
 function* rootSaga() {
-  yield all([adminSaga(), authSaga(), fieldSaga(), loginUserSaga()]);
+  yield all([
+    adminSaga(),
+    authSaga(),
+    fieldSaga(),
+    loginUserSaga(),
+    homeworkSaga(),
+  ]);
 }
 
 export default rootSaga;
