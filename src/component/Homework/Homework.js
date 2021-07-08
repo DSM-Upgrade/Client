f373eaf80e401af5a6bbe25e7b0f5fe7cb5d2f09
JaeHyun <soehd0889@naver.com>
2021-07-08 20:15:10 +0900

    [MODIFY] <src/component/Homework/Homework.js> modify  code

--- src/component/Homework/Homework.js
+++ src/component/Homework/Homework.js
@@ -10,76 +10,76 @@ const Homework = (props) => {
   console.log(`props`);
   console.log(homeworkList);
 
-  const HomeworkList =
-    homeworkList.length &&
-    homeworkList.map((homeworkList, index) => {
-      console.log(homeworkList);
-      const id = homeworkList.id;
-      if (homeworkList.status === "ASSIGNED") {
-        return (
-          <Link
-            key={index}
-            to={{
-              pathname: `/homeworkContent/${id}`,
-              state: {
-                Id: id,
-              },
-            }}
-          >
-            <S.AllocationBox>
-              <h1>{homeworkList.title}</h1>
-              <p>{homeworkList.content}</p>
-              <p>분야 : 디자인</p>
-              <p>대상 : 동아리원 전체</p>
-              <p>기한 : {homeworkList.deadline} 까지 제출</p>
-              <h4>할당됨</h4>
-            </S.AllocationBox>
-          </Link>
-        );
-      } else if (homeworkList.status === "SUBMITTED") {
-        return (
-          <Link
-            key={index}
-            to={{
-              pathname: `/homeworkContent/${id}`,
-              state: {
-                Id: id,
-              },
-            }}
-          >
-            <S.SubmissionBox>
-              <h1>{homeworkList.title}</h1>
-              <p>{homeworkList.content}</p>
-              <p>분야 : 디자인</p>
-              <p>대상 : 동아리원 전체</p>
-              <p>기한 : {homeworkList.deadline} 까지 제출</p>
-              <h4>제출함</h4>
-            </S.SubmissionBox>
-          </Link>
-        );
-      } else if (homeworkList.status === "UNSUBMITTED") {
-        return (
-          <Link
-            key={index}
-            to={{
-              pathname: `/homeworkContent/${id}`,
-              state: {
-                Id: id,
-              },
-            }}
-          >
-            <S.UnsubmittedBox>
-              <h1>{homeworkList.title}</h1>
-              <p>{homeworkList.content}</p>
-              <p>분야 : 디자인</p>
-              <p>대상 : 동아리원 전체</p>
-              <p>기한 : 제출마감</p>
-              <h4>미제출</h4>
-            </S.UnsubmittedBox>
-          </Link>
-        );
-      }
-    });
+  const HomeworkList = homeworkList.length
+    ? homeworkList.map((homeworkList, index) => {
+        console.log(homeworkList);
+        const id = homeworkList.id;
+        if (homeworkList.status === "ASSIGNED") {
+          return (
+            <Link
+              key={index}
+              to={{
+                pathname: `/homeworkContent/${id}`,
+                state: {
+                  Id: id,
+                },
+              }}
+            >
+              <S.AllocationBox>
+                <h1>{homeworkList.title}</h1>
+                <p>{homeworkList.content}</p>
+                <p>분야 : 디자인</p>
+                <p>대상 : 동아리원 전체</p>
+                <p>기한 : {homeworkList.deadline} 까지 제출</p>
+                <h4>할당됨</h4>
+              </S.AllocationBox>
+            </Link>
+          );
+        } else if (homeworkList.status === "SUBMITTED") {
+          return (
+            <Link
+              key={index}
+              to={{
+                pathname: `/homeworkContent/${id}`,
+                state: {
+                  Id: id,
+                },
+              }}
+            >
+              <S.SubmissionBox>
+                <h1>{homeworkList.title}</h1>
+                <p>{homeworkList.content}</p>
+                <p>분야 : 디자인</p>
+                <p>대상 : 동아리원 전체</p>
+                <p>기한 : {homeworkList.deadline} 까지 제출</p>
+                <h4>제출함</h4>
+              </S.SubmissionBox>
+            </Link>
+          );
+        } else if (homeworkList.status === "UNSUBMITTED") {
+          return (
+            <Link
+              key={index}
+              to={{
+                pathname: `/homeworkContent/${id}`,
+                state: {
+                  Id: id,
+                },
+              }}
+            >
+              <S.UnsubmittedBox>
+                <h1>{homeworkList.title}</h1>
+                <p>{homeworkList.content}</p>
+                <p>분야 : 디자인</p>
+                <p>대상 : 동아리원 전체</p>
+                <p>기한 : 제출마감</p>
+                <h4>미제출</h4>
+              </S.UnsubmittedBox>
+            </Link>
+          );
+        }
+      })
+    : "리스트 불러오는중...";
 
   return (
     <S.Container>
