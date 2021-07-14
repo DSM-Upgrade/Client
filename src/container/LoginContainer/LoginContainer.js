6ad097cc36c2e75ae06f0d90484a2f41f4c6daf7
JaeHyun <soehd0889@naver.com>
2021-07-14 14:29:22 +0900

    [MODIFY] <src/container/LoginContainer> delete code

--- src/container/LoginContainer/LoginContainer.js
+++ src/container/LoginContainer/LoginContainer.js
@@ -6,21 +6,14 @@ import { loginPageActionsCreaters } from "../../module/action/loginPage/index";
 
 import LoginPage from "../../component/LoginPage/LoginPage";
 
-import { setItem } from "../../utils/LocalStorageUtils";
-
 const LoginContainer = () => {
   const dispatch = useDispatch();
   const loginInfo = useSelector((state) => state.loginPage.loginInfo);
-  const authToken = useSelector((state) => state.loginPage.authToken);
   const inputStatus = useSelector((state) => state.loginPage.inputStatus);
 
   const [loginData, setLoginData] = useState({
     ...loginInfo,
   });
-
-  const { access_token, refresh_token } = authToken;
-  setItem("accessToken", access_token);
-  setItem("refreshToken", refresh_token);
 
   const { authLogInSaga, setLoginInputNull } = loginPageActionsCreaters;
 
