2557ef7a60178b232623b0432f409f6e45eb2d56
JaeHyun <soehd0889@naver.com>
2021-07-14 11:03:28 +0900

    [FEAT] <src/~/HomeworkCreate> add assignment api

--- src/component/Homework/HomeworkCreate/HomeworkCreate.js
+++ src/component/Homework/HomeworkCreate/HomeworkCreate.js
@@ -1,37 +1,103 @@
-import React, { useState } from "react";
+import React from "react";
 import * as S from "./style";
 import { Link } from "react-router-dom";
 import HeaderContainer from "../../../container/HeaderContainer/HeaderContainer";
-import TitleHeaderContainer from "../../../container/TitleHeaderContainer/TitleHeaderContainer";
 
-const HomeworkAdmin = () => {
+const HomeworkAdmin = (props) => {
+  const {
+    onSubmitEvent,
+    onChangeFormData,
+    userList,
+    changeHandler,
+    assignmentObj,
+  } = props;
+
+  const { title, content, date, time } = assignmentObj;
+
+  const UserList = userList.length ? (
+    userList.map((userList, index) => {
+      let field_name = "";
+      const { name, username, student_num, field_id } = userList;
+      if (field_id === 1) {
+        field_name = "백엔드";
+      } else if (field_id === 2) {
+        field_name = "디자인";
+      } else if (field_id === 3) {
+        field_name = "프론트엔드";
+      }
+      return (
+        <div key={index}>
+          <input
+            type="checkbox"
+            name="usernames"
+            onChange={(e) => {
+              changeHandler(e.currentTarget.checked, username);
+            }}
+          />
+          <p>{student_num}</p>
+          <p>
+            {name}({username})
+          </p>
+          <p>{field_name}</p>
+        </div>
+      );
+    })
+  ) : (
+    <div style={{ paddingLeft: "40%" }}>리스트 불러오는 중...</div>
+  );
+
   return (
     <S.Container>
       <HeaderContainer />
       <S.MainWrapper>
         <S.SectionContainer>
-          <S.SectionHeaderContainer>
+          <S.SectionHeaderContainer onSubmit={onSubmitEvent}>
             <header>
-              <input type="text" placeholder="숙제 제목 입력"></input>
+              <input
+                type="text"
+                placeholder="숙제 제목 입력"
+                name="title"
+                value={title || ''}
+                onChange={onChangeFormData}
+              ></input>
               <div className="dayWrapper">
                 <p>기한 : </p>
-                <input type="date"></input>
-                <input type="time"></input>
+                <input
+                  type="date"
+                  name="date"
+                  value={date || ''}
+                  onChange={onChangeFormData}
+                ></input>
+                <input
+                  type="time"
+                  name="time"
+                  value={time || ''}
+                  onChange={onChangeFormData}
+                ></input>
               </div>
             </header>
             <section>
-              <textarea placeholder="설명을 써 넣어주세요"></textarea>
-              <div>
-                <p>할당 멤버 선택</p>
-                <input></input>
+              <textarea
+                placeholder="설명을 써 넣어주세요"
+                name="content"
+                value={content || ''}
+                onChange={onChangeFormData}
+              ></textarea>
+              <div className="checkMember">
+                <p className="memberTitle">할당 멤버 선택</p>
+                <div className="checkWrapper">{UserList}</div>
               </div>
-              <input type="button" value="생성하기" />
+              <input type="submit" value="생성하기" />
             </section>
           </S.SectionHeaderContainer>
         </S.SectionContainer>
       </S.MainWrapper>
     </S.Container>
   );
+};
+
+HomeworkAdmin.defaultProps = {
+  userList: [],
 };
 
 export default HomeworkAdmin;
--- src/component/Homework/HomeworkCreate/style.js
+++ src/component/Homework/HomeworkCreate/style.js
@@ -31,7 +31,7 @@ export const SectionContainer = styled.div`
   flex-direction: column;
 `;
 
-export const SectionHeaderContainer = styled.div`
+export const SectionHeaderContainer = styled.form`
   width: 100%;
   height: 74px;
   border-bottom: 1px solid #000000;
@@ -72,16 +72,41 @@ export const SectionHeaderContainer = styled.div`
         color: black;
       }
     }
-    div > p {
+    .memberTitle {
       font-size: 20px;
       margin-bottom: 16px;
     }
-    div > input {
+    .checkWrapper {
       width: 810px;
       height: 315px;
       margin-bottom: 60px;
+      background-color: #fff;
+      border: 1px solid black;
+      overflow-y: scroll;
+      overflow-x: none;
+      -ms-overflow-style: none; /* IE and Edge */
+      scrollbar-width: none; /* Firefox */
+      &::-webkit-scrollbar {
+        display: none; /* Chrome, Safari, Opera*/
+      }
+      input[type="checkbox"] {
+        width: 15px;
+        height: 15px;
+        margin: 0 33px 0 28px;
+      }
+      div {
+        width: 100%;
+        height: 56px;
+        display: flex;
+        align-items: center;
+        font-size: 20px;
+        p:nth-child(3) {
+          margin: 0 19px 0 19px;
+        }
+        border-bottom: #707070 1px solid;
+      }
     }
-    input[type="button"] {
+    input[type="submit"] {
       width: 139px;
       height: 52px;
       font-size: 20px;
