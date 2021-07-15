ed6b8fa7faedb7136f41af062c7ec7944d1891a1
JaeHyun <soehd0889@naver.com>
2021-07-15 11:21:00 +0900

    [FEAT] <src/container/HomeworkContainer/HomeworkAdminContainer> add code

--- src/container/HomeworkContainer/HomeworkAdminContainer/HomeworkAdminContainer.js
+++ src/container/HomeworkContainer/HomeworkAdminContainer/HomeworkAdminContainer.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import HomeworkAdmin from "../../../component/Homework/HomeworkAdmin/HomeworkAdmin";
+import { homeworkActionsCreaters } from "../../../module/action/homework";
 
 const HomeworkAdminContainer = () => {
-  return <HomeworkAdmin />;
+  const dispatch = useDispatch();
+
+  const adminList = useSelector((state) => state.homework.adminHomeworkList);
+
+  const { getAdminHomeworkListSaga } = homeworkActionsCreaters;
+
+  const getHomeworkAdminList = () => {
+    dispatch(getAdminHomeworkListSaga());
+  };
+
+  useEffect(() => {
+    getHomeworkAdminList();
+  }, []);
+
+  return <HomeworkAdmin adminList={adminList} />;
 };
 
 export default HomeworkAdminContainer;
